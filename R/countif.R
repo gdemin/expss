74587f92ad2cac6579e429786dfbe08e10e8eb24
Gregory Demin <gdemin+github@gmail.com>
2014-11-27 04:59:49 +0300

    criteria -> criterion

--- R/countif.R
+++ R/countif.R
@@ -4,18 +4,18 @@
 #' similar to Microsoft Excel \code{COUNTIF}. The second works rowwise - e. g. 
 #' similar to SPSS \code{COUNT} function. 
 #'  
-#' @param criteria Vector with counted values or function (possibly constructed with \code{crit}) 
+#' @param criterion Vector with counted values or function (possibly constructed with \code{crit}) 
 #' @param ... Tested data. Vector, matrix, data.frame, list. Shorter arguments will be recycled.
-#' @param FUN Function for criteria construction. In most cases it is '>','==' and so on.
+#' @param FUN Function for criterion construction. In most cases it is '>','==' and so on.
 #' @param x Tested data. Vector, matrix, data.frame, list. Shorter columns in list
 #'  will be recycled.
 #' 
 #' @return 
 #' \code{countif} return single value (vector of length 1). 
 #' \code{row_countif} returns vector of counts for each row of supplied arguments.
-#' \code{\%has\%} returns logical vector - presense indicator of criteria in each row.
-#' \code{crit} returns function of class 'criteria' that test argument for 
-#' supplied criteria. 
+#' \code{\%has\%} returns logical vector - presense indicator of criterion in each row.
+#' \code{crit} returns function of class 'criterion' that test argument for 
+#' supplied condition. 
 #' 
 #' @details
 #' \code{countif} counts values in entire dataset and return single 
@@ -24,13 +24,13 @@
 #' \code{row_countif} counts values in each row of supplied arguments and return
 #' vector of counts for each row of supplied arguments.
 #' 
-#' Both functions never return NA's. If criteria is missing (or is NULL) 
+#' Both functions never return NA's. If criterion is missing (or is NULL) 
 #' non-NA's values will be counted. 
 #' 
 #' \code{crit(FUN,...)} translate FUN in function \code{FUN(x,...)}. Functions 
 #' returned by \code{crit} may be combined with logical operators: |, &, !.
 #' 
-#' \code{\%has\%} is simple wrapper for rather frequent case \code{row_countif(criteria,x)>0}.
+#' \code{\%has\%} is simple wrapper for rather frequent case \code{row_countif(criterion,x)>0}.
 #' 
 #' @export
 #' @examples
@@ -41,15 +41,22 @@
 #' )
 #' 
 #' countif("apples",df1$a) # 2
+#' 
 #' countif("apples",df1) # 2
+#' 
 #' with(df1,countif("apples",a,b)) # 2
+#' 
 #' countif(crit(">",55),df1$b) # 2
+#' 
 #' countif(crit("!=",75),df1$b) # 3
+#' 
 #' countif(crit(">=",32),df1$b) # 4
+#' 
 #' countif(crit(">",32) & crit("<",86),df1$b) # 2
+#' 
 #' countif(33:85,df1$b) # 2
 #' 
-#' # more complex criteria
+#' # more complex criterion
 #' # values with letters
 #' countif(function(x) grepl("^[A-z]+$",x),df1) # 4
 #' 
@@ -58,6 +65,7 @@
 #' 
 #' # row_countif
 #' row_countif(function(x) grepl("^a",x),df1) # c(1,0,0,1)
+#' 
 #' df1 %has% 'apples' # c(TRUE,FALSE,FALSE,TRUE)
 #' 
 #' # example with dplyr
@@ -67,27 +75,29 @@
 #' )
 #' df2  %>% mutate(exact = row_countif(8,V1,V2,V3),
 #'                 greater = row_countif(crit(">",8),V1,V2,V3),
-#'                 range = row_countif(5:8,V1,V2,V3)
+#'                 range = row_countif(5:8,V1,V2,V3),
+#'                 na = row_countif(is.na,V1,V2,V3),
+#'                 not_na = row_countif(,V1,V2,V3)
 #'                 ) -> result
 #' result
-countif=function(criteria=NULL,...){
+countif=function(criterion=NULL,...){
     dtfrm = do.call(data.frame,list(...)) # form data.frame to use rowSums 
-    cond = build_criteria(criteria,dtfrm)
+    cond = build_criterion(criterion,dtfrm)
     sum(cond,na.rm=TRUE)
 }
 
 #' @export
 #' @rdname countif
-row_countif=function(criteria=NULL,...){
+row_countif=function(criterion=NULL,...){
     dtfrm = do.call(data.frame,list(...)) # form data.frame to use rowSums 
-    cond = build_criteria(criteria,dtfrm)
+    cond = build_criterion(criterion,dtfrm)
     rowSums(cond,na.rm=TRUE)
 }
 
 #' @export
 #' @rdname countif
-'%has%'=function(x,criteria){
-    row_countif(criteria=criteria,x)>0
+'%has%'=function(x,criterion){
+    row_countif(criterion=criterion,x)>0
 }
 
 #' @export
@@ -95,44 +105,58 @@ row_countif=function(criteria=NULL,...){
 crit = function(FUN,...){
     FUN <- match.fun(FUN)
     res = function(x) FUN(x,...)
-    class(res) = unique("criteria",class(res))
+    class(res) = unique("criterion",class(res))
     res
 }
 
 #' @export
-'!.criteria' = function(a) function(x) !a(x)
+'!.criterion' = function(a) {
+    res = function(x) !a(x)
+    class(res) = unique("criterion",class(res))
+    res
+}
 
 
 #' @export
-'|.criteria' = function(e1,e2) function(x) e1(x) | e2(x)
+'|.criterion' = function(e1,e2) {
+    res = function(x) e1(x) | e2(x)
+    class(res) = unique("criterion",class(res))
+    res
+}
+
 
 #' @export
-'&.criteria' = function(e1,e2) function(x) e1(x) & e2(x)
+'&.criterion' = function(e1,e2) {
+    res = function(x) e1(x) & e2(x)
+    class(res) = unique("criterion",class(res))
+    res
+}
 
 
-build_criteria = function(criteria,dtfrm){
-    if (is.null(criteria)){
+
+build_criterion = function(criterion,dtfrm){
+    if (is.null(criterion)){
         cond = !is.na(dtfrm)
         return(cond)
     }
-    UseMethod("build_criteria")
+    UseMethod("build_criterion")
 }
 
-build_criteria.function = function(criteria,dtfrm){
+build_criterion.function = function(criterion,dtfrm){
     res = lapply(dtfrm,function(colmn){
-        cond = criteria(colmn)
-        stopifnot_message(length(cond)==length(colmn),"Cells number of criteria doesn't equal cells number of argument.")
+        cond = criterion(colmn)
+        stopifnot_message(length(cond)==length(colmn),"Cells number of criterion doesn't equal cells number of argument.")
         as.logical(cond)
     })
     do.call(cbind,res)
 }
 
-build_criteria.default = function(criteria,dtfrm){
-    build_criteria.function(function(x) x %in% criteria,dtfrm)
+build_criterion.default = function(criterion,dtfrm){
+    build_criterion.function(function(x) x %in% criterion,dtfrm)
 }
 
-build_criteria.criteria = function(criteria,dtfrm){
-    build_criteria.function(criteria,dtfrm)
+build_criterion.criterion = function(criterion,dtfrm){
+    build_criterion.function(criterion,dtfrm)
 }
 
 
