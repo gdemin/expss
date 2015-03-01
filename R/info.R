bc3d289041f45118b71a42c458e556407c9f9ced
Gregory Demin <gdemin+github@gmail.com>
2015-03-01 21:24:35 +0300

    Add docs for 'info'

--- R/info.R
+++ R/info.R
@@ -1,10 +1,27 @@
+#' Provide variables info for dataset.
+#' 
+#' \code{into} returns data.frame with variables info. 
+#' 
+#' @param x vector/factor/list/data.frame.
+#' @param frequencies Logical. Should we calculate frequencies for each variable? This calculation can
+#' take significant amount of time for large datasets.
+#' @param max_levels Numeric. Maximum levels for using in frequency calculations. Levels above 
+#' this value will convert to 'Other values'.
+#' @return data.frame with following columns: Name, Class, Length, NotNA, Label, ValueLabels, Frequency.
+#' @details Resulting data.frame of this function mainly intended to be saved as csv to keep before eyes
+#' in RStudio viewer or spreadsheet software as reference about working dataset. 
+#' @examples
+#' data(mtcars)
+#' var_lab(mtcars$am) = "Transmission"
+#' val_lab(mtcars$am) = c("Automatic"=0, "Manual"=1)
+#' info(mtcars,max_levels = 5)                             
 #' @export
-info=function(x, frequencies = TRUE, max_levels= 30){
+info=function(x, frequencies = TRUE, max_levels= 10){
     UseMethod("info")
 }
 
 #' @export
-info.default=function(x, frequencies = TRUE, max_levels= 30){
+info.default=function(x, frequencies = TRUE, max_levels= 10){
     max_levels = max(1,max_levels)
     varlab=var_lab(x)
     if (length(varlab)==0) varlab=NA
@@ -20,14 +37,14 @@ info.default=function(x, frequencies = TRUE, max_levels= 30){
                "Label"=varlab,
                "ValueLabels"=vallab)
     if (frequencies){
-        values=table(x,useNA="ifany")
+        values=sort(table(x,useNA="ifany"),na.last = TRUE,decreasing = TRUE)
         if (NA %in% names(values)) values=c(tail(values,1),head(values,-1))
         if (length(values)>max_levels) {
             other=sum(values[-(1:max_levels)])
             values=values[1:max_levels]
             values["Other values"]=other
         }
-        values=paste(paste(names(values),values,sep="="),collapse=", ")
+        values=paste(paste(names(values),values,sep=":"),collapse=", ")
         res = c(res, Frequency =values)
     }
     curr_name = paste(as.character(substitute(x)),collapse = "")
@@ -35,16 +52,16 @@ info.default=function(x, frequencies = TRUE, max_levels= 30){
 }
 
 #' @export
-info.data.frame=function(x, frequencies = TRUE, max_levels= 30){
+info.data.frame=function(x, frequencies = TRUE, max_levels= 10){
     info.list(x,frequencies,max_levels)
 }
 
-info.matrix=function(x, frequencies = TRUE, max_levels= 30){
+info.matrix=function(x, frequencies = TRUE, max_levels= 10){
     info.list(as.data.frame(x,stringsAsFactors = FALSE),frequencies,max_levels)
 }
 
 #' @export
-info.list=function(x, frequencies = TRUE, max_levels= 30){
+info.list=function(x, frequencies = TRUE, max_levels= 10){
     res = lapply(x,function(y) info(y,frequencies,max_levels))
     res = do.call(rbind,res)
     rownames(res) = seq_len(nrow(res))                  
