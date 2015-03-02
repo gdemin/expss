0fc44c3cac99bf6bc61ca4fe96f246168efa0dd7
Gregory Demin <gdemin+github@gmail.com>
2015-03-02 21:51:24 +0300

    Add tests and docs for category.

--- R/category.R
+++ R/category.R
@@ -1,23 +1,25 @@
-#' Convert variable (possibly multiple choice question) to matrix of dummy variables.
+#' Convert dichotomy matrix to matrix with category encoding.
 #' 
-#' \code{dichotomy} returns matrix with 0,1 and possibly NA. 
-#' \code{dichotomy1} returns same result as \code{dichotomy} but without last category.
+#' @param x Dichotomy matrix (usually with 0,1 coding).
+#' @param counted_value Vector. Values that should be considered as indicator 
+#' of category presence. By default it is 1.
+#' @param compress Logical. Should we drop columns with all NA?
+#' @return Matrix with numeric values that correspond to column numbers of counted values. 
+#' Column names of x are added as value labels.
+#' @seealso \code{\link{dichotomy}} for reverse conversion.
+#' @examples
+#' set.seed(123)
 #' 
-#' @param x vector/factor/matrix/data.frame.
-#' @param keep_unused Logical. Should we create columns for unused value labels/factor levels.
-#' @param use_na Logical. Should we use NA for rows with all NA or use 0's instead.
-#' @param keep Numeric/character. Labels/levels/values that should be kept.
-#' By default all values will be kept.
-#' @return matrix with 0,1 which column names are value labels. If label doesn't exist for 
-#' particular value then this value will be used as column name.
-#' @details This function converts variable/multiple response variable(matrix/data.frame)
-#'  with category encoding into matrix with dichotomy encoding (0/1) 
-#' suited for most statistical analysis, e. g. clustering, factor analysis, 
-#' linear regression and so on.  
-#' \code{dichotomy1} drops last column in dichotomy matrix. It is useful in many cases
-#' because any column of such matrix usually is linear combinations of other columns.
-#' @seealso \code{\link{category}} for reverse conversion.
-#' examples
+#' # Let's imagine it's matrix of consumed products
+#' dichotomy_matrix = matrix(sample(0:1,40,replace = TRUE,prob=c(.6,.4)),nrow=10)
+#' colnames(dichotomy_matrix) = c("Milk","Sugar","Tea","Coffee")
+#' category(dichotomy_matrix,compress=FALSE) # uncompressed version
+#' category_matrix=category(dichotomy_matrix)
+#' 
+#'  # should be TRUE
+#' identical(val_lab(category_matrix),c(Milk = 1L,Sugar = 2L,Tea = 3L,Coffee = 4L))
+#' all(dichotomy(category_matrix,use_na = FALSE)==dichotomy_matrix)
+#' 
 #' @export
 category = function(x, counted_value=1, compress = TRUE){
     UseMethod("category")    
@@ -28,7 +30,7 @@ category.matrix = function(x, counted_value=1, compress = TRUE){
    vallab = colnames(x)
    res = col(x)
    res[!(x %in% counted_value)] = NA
-   if(compress){
+   if(compress && length(res)>0 && ncol(res)>1){
        res = t(apply(res,1,sort,na.last = TRUE))
        columns_with_values = colSums(!is.na(res))>0
        res = res[,columns_with_values,drop = FALSE]
@@ -37,6 +39,7 @@ category.matrix = function(x, counted_value=1, compress = TRUE){
        val_lab(res) = structure(seq_len(ncol(x)),names = vallab) 
        
    } 
+   class(res) = setdiff(class(res),"dichotomy")
    res
 }
 
---
+++ tests/testthat/test_category.R
@@ -0,0 +1,27 @@
+context("category")
+
+set.seed(123)
+dichotomy_matrix = matrix(sample(0:1,40,replace = TRUE,prob=c(.6,.4)),nrow=10)
+colnames(dichotomy_matrix) = c("Milk","Sugar","Tea","Coffee")
+expect_equal_to_reference(category(dichotomy_matrix,compress=FALSE),"rds/category1.rds")
+
+category_matrix=category(dichotomy_matrix)
+expect_identical(val_lab(category_matrix),c(Milk = 1L,Sugar = 2L,Tea = 3L,Coffee = 4L))
+
+class(dichotomy_matrix) = union("dichotomy",class(dichotomy_matrix))
+expect_identical(dichotomy(category_matrix,use_na = FALSE),dichotomy_matrix*1.0)
+
+
+dichotomy_matrix[] = 0
+expect_equal_to_reference(category(dichotomy_matrix),"rds/category2.rds")
+
+expect_equal_to_reference(category(dichotomy_matrix, compress = FALSE),"rds/category3.rds")
+
+
+expect_identical(category(dichotomy_matrix[,FALSE,drop = FALSE], compress = FALSE),structure(integer(0), .Dim = c(10L, 0L)))
+expect_identical(category(dichotomy_matrix[,FALSE,drop = FALSE]),structure(integer(0), .Dim = c(10L, 0L)))
+expect_identical(category(numeric(0)),structure(integer(0), .Dim = 0:1))
+
+expect_identical(category(t(t(c(0,1,0,1,0,1)))),structure(c(NA, 1L, NA, 1L, NA, 1L), .Dim = c(6L, 1L)))
+expect_identical(category(t(c(0,1,0,1,0,1))), structure(c(2L, 4L, 6L), .Dim = c(1L, 3L)))
+expect_identical(category(c(0,1,0,1,0,1)),structure(c(NA, 1L, NA, 1L, NA, 1L), .Dim = c(6L, 1L)))
