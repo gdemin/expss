7e92f73980cd5a199309f9e80b3fd2f214854304
Gregory Demin <gdemin+github@gmail.com>
2016-04-19 17:04:30 +0300

    Modify set_na help

--- R/set_na.R
+++ R/set_na.R
@@ -2,16 +2,19 @@
 #' Replace certain values with NA
 #' 
 #' @details 
-#' There are following options for \code{value} 
+#' There are following options for \code{value}: 
 #' \itemize{ 
-#' \item{vector/matrix/data.frame/list.}{ Most
-#' simple case - vector - all values from this vector will be replaced with
-#' NA's. If value is matrix/vector/data.frame values will be replaced from appropriate columns.} 
-#' \item{logical vector/matrix/data.frame/list.}{ NA's will be set in places where value is TRUE.}
-#' \item{function.}{ NA's will be set in places where value(x) is TRUE. You can use special functions. 
-#' For example \code{set_na(a, gt(98))} will replace all values greater 98 in \code{a} with NA.
-#' If \code{x} has many columns(rows) and \code{value} has one column(row) then it will be recycled. 
-#' List of special functions see below.}
+#' \item{vector/matrix/data.frame/list.}{ Vector of values which should be
+#' replaced with \code{NA} in \code{x}. If value is matrix/vector/data.frame then for
+#' each column of \code{x} will be used appropriate column of \code{value}.}
+#' \item{logical vector/matrix/data.frame/list.}{ NA's will be set in places
+#' where \code{value} is TRUE.}
+#' \item{function.}{ NA's will be set in places where \code{value(x)} is TRUE.
+#' Additionaly there are special functions for common cases of comparison with
+#' value. For example \code{set_na(my_var, gt(98))} will replace all values greater
+#' 98 in \code{my_var} with NA. If \code{x} has many columns(rows) and \code{value}
+#' has one column(row) then it will be recycled. List of special functions see
+#' below.}
 #' }
 #' Special functions for usage as criteria:
 #' \itemize{
@@ -28,6 +31,9 @@
 #' @param value vector/matrix/data.frame/list/function
 #'   
 #' @return x with NA's instead of \code{value}
+#' 
+#' @seealso For reverse operation see \link{if_na}, \link{if_val} for more
+#'   general recodings.
 #'   
 #' @examples
 #' a = c(1:5,99)
@@ -40,13 +46,15 @@
 #'       b = runif(5)
 #' )
 #' 
+#' # rows with 'bad value' will be filled with NA
+#' # logical argument and recycling by columns
 #' set_na(dfs, dfs$a=="bad value")
 #' 
-#' a = runif(50)
+#' a = rnorm(50)
+#' # values greater than 1 or less than -1 will be set to NA
+#' # special functions usage
 #' set_na(a, lt(-1) | gt(1))
 #' 
-#' @seealso For reverse operation see \link{if_na}, \link{if_val} for more
-#'   general recodings.
 #' @export
 set_na = function(x, value){
     if_val(x, from=list(value), to = list(NA))
