8554512b6b0572e0a23dde3ce6f440641e1b3a28
Gregory Demin <gdemin+github@gmail.com>
2016-04-19 17:16:23 +0300

    Make tests with dplyr/magrittr conditional on these packages availability

--- tests/testthat/test_if_val.R
+++ tests/testthat/test_if_val.R
@@ -22,38 +22,37 @@ expect_identical(if_val(x, list(gt(2)~y, lte(2) ~ z, .~99)), c(4, 8, 4, 9, 99))
 
 expect_identical(if_val(x, (z>4)~y), c(1, 3, 1, 9, 9))
 
-context("if_val dplyr")
-library(dplyr)
-library(magrittr)
-
-
-x = c(1,3,1,3,NA)
-y = c(8,8,8,9,9)
-z = c(4,4,4,5,5)
-
-dfs = data.frame(
-    x = c(2,4,2,4,NA),
-    y = c(18,18,18,19,19),
-    z = c(14,14,14,15,15)
+if (require(dplyr) && require(magrittr)){
+    context("if_val dplyr")
     
-)
-
-dfs  %<>% mutate(
-    w = if_val(x,gt(2)~y)
-)
-
-expect_identical(dfs$w, c(2, 18, 2, 19, NA))
-
-dfs$x = NULL
-dfs$w = NULL
-dfs  %<>% mutate(
-    w = if_val(x, gt(2)~y)
-)
-expect_identical(dfs$w, c(1, 18, 1, 19, NA))
-
+    
+    x = c(1,3,1,3,NA)
+    y = c(8,8,8,9,9)
+    z = c(4,4,4,5,5)
+    
+    dfs = data.frame(
+        x = c(2,4,2,4,NA),
+        y = c(18,18,18,19,19),
+        z = c(14,14,14,15,15)
+        
+    ) %>% tbl_df()
+    
+    dfs  %<>% mutate(
+        w = if_val(x,gt(2)~y)
+    )
+    
+    expect_identical(dfs$w, c(2, 18, 2, 19, NA))
+    
+    dfs$x = NULL
+    dfs$w = NULL
+    dfs  %<>% mutate(
+        w = if_val(x, gt(2)~y)
+    )
+    expect_identical(dfs$w, c(1, 18, 1, 19, NA))
+}
 ##########################
 
-context("if_val from, to notation simple vector")
+context("if_val 'from, to' notation simple vector")
 
 expect_identical(if_val(1:5, from = 1, to = -1), c(-1, 2, 3, 4, 5))
 expect_identical(if_val(1:5, from = 1:2, to =c(-1, NA)), c(-1, NA, 3, 4, 5))
@@ -76,35 +75,34 @@ expect_identical(if_val(x, from = list(gt(2), lte(2),"."), to = list(y,z,99)), c
 
 expect_identical(if_val(x, from = list(z>4), to = list(y)), c(1, 3, 1, 9, 9))
 
-context("if_val from, to notation dplyr")
-library(dplyr)
-library(magrittr)
+context("if_val 'from, to' notation dplyr")
 
-
-x = c(1,3,1,3,NA)
-y = c(8,8,8,9,9)
-z = c(4,4,4,5,5)
-
-dfs = data.frame(
-    x = c(2,4,2,4,NA),
-    y = c(18,18,18,19,19),
-    z = c(14,14,14,15,15)
+if (require(dplyr) && require(magrittr)){
     
-)
-
-dfs  %<>% mutate(
-    w = if_val(x, from = list(gt(2)), to = list(y))
-)
-
-expect_identical(dfs$w, c(2, 18, 2, 19, NA))
-
-dfs$x = NULL
-dfs$w = NULL
-dfs  %<>% mutate(
-    w = if_val(x, from = list(gt(2)), to = list(y))
-)
-expect_identical(dfs$w, c(1, 18, 1, 19, NA))
-
+    x = c(1,3,1,3,NA)
+    y = c(8,8,8,9,9)
+    z = c(4,4,4,5,5)
+    
+    dfs = data.frame(
+        x = c(2,4,2,4,NA),
+        y = c(18,18,18,19,19),
+        z = c(14,14,14,15,15)
+        
+    )
+    
+    dfs  %<>% mutate(
+        w = if_val(x, from = list(gt(2)), to = list(y))
+    )
+    
+    expect_identical(dfs$w, c(2, 18, 2, 19, NA))
+    
+    dfs$x = NULL
+    dfs$w = NULL
+    dfs  %<>% mutate(
+        w = if_val(x, from = list(gt(2)), to = list(y))
+    )
+    expect_identical(dfs$w, c(1, 18, 1, 19, NA))
+}
 
 
 
