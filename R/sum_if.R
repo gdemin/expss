077314ae7530037b85f39930993e87057f1035d5
Gregory Demin <gdemin+github@gmail.com>
2016-04-20 19:14:54 +0300

    Add sum_if and tests for it. No docs yet.

---
+++ R/sum_if.R
@@ -0,0 +1,170 @@
+#' Conditional sum/mean/max/min
+#' 
+#' There are two flavors of this function - one works with entire dataset/matrix/vector
+#' similar to Microsoft Excel \code{COUNTIF}. The second works rowwise - e. g. 
+#' similar to SPSS \code{COUNT} function. 
+#'  
+#' @param criterion Vector with counted values, list with conditions or
+#'   function. If criterion is missing (or is NULL) non-NA's values will be
+#'   counted.
+#' @param ... data. Vectors, matrixes, data.frames, list. Shorter arguments
+#'   will be recycled.
+#' @param x Counted values or criterion for counting. Vector, matrix, data.frame,
+#'   list, function. Shorter columns in list will be recycled.
+#' 
+#' @return 
+#' \code{count_if} return single value (vector of length 1). 
+#' \code{row_count_if} returns vector of counts for each row of supplied arguments.
+#' \code{col_count_if} returns vector of counts for each column of supplied arguments.
+#' \code{\%has\%} returns logical vector - presence indicator of criterion in each row.
+#' 
+#' @details
+#' \code{count_if} counts values in entire dataset and return single 
+#' value (vector of length 1).
+#' 
+#' \code{row_count_if} counts values in each row of supplied arguments and return
+#' vector of counts for each row of supplied arguments.
+#' 
+#' \code{col_count_if} counts values in each column of supplied arguments and return
+#' vector of counts for each column of supplied arguments.
+#' 
+#' All functions never return NA's. 
+#' 
+#' If criterion is list, then each element considered as single condition and
+#' they will be combined with AND.
+#' 
+#' Function criterion should return logicals of same size and shape as its argument.
+#' This function will be applied to each column of supplied data and TRUE results will be counted.
+#' There is asymmetrical behavior in \code{row_count_if} and
+#' \code{col_count_if}: in both cases function criterion will be applied
+#' columnwise.
+#' There are special functions for usage as criteria (e. g. \code{gt(5)} is
+#' equivalent ">5" in spreadsheet):
+#' \itemize{
+#' \item{\code{gt}}{ greater than}
+#' \item{\code{gte}}{ greater than or equal}
+#' \item{\code{eq}}{ equal} 
+#' \item{\code{neq}}{ not equal} 
+#' \item{\code{lt}}{ less than}
+#' \item{\code{lte}}{ less than or equal}
+#' } 
+#' 
+#' \code{\%has\%} is simple wrapper for rather frequent case \code{row_count_if(criterion,x)>0}.
+#' 
+#' @export
+#' @examples
+#' # Examples borrowed from Microsoft Excel help for COUNTIF
+#' df1 = data.frame(
+#'     a=c("apples",   "oranges",     "peaches",     "apples"),
+#'     b = c(32, 54, 75, 86)
+#' )
+#' 
+#' count_if("apples",df1$a) # 2
+#' 
+#' count_if("apples",df1) # 2
+#' 
+#' with(df1,count_if("apples",a,b)) # 2
+#' 
+#' count_if(gt(55),df1$b) # greater than 55 = 2
+#' 
+#' count_if(neq(75),df1$b) # not equal 75 = 3
+#' 
+#' count_if(gte(32),df1$b) # greater than or equal 32 = 4
+#' 
+#' count_if(list(gt(32), lt(86)),df1$b) # 2
+#' 
+#' count_if(gt(32) & lt(86),df1$b) # 2
+#' 
+#' count_if(33:85,df1$b) # 2
+#' 
+#' # more complex criteria
+#' # values with letters
+#' count_if(function(x) grepl("^[A-z]+$",x),df1) # 4
+#' 
+#' # values that started on 'a'
+#' count_if(function(x) grepl("^a",x),df1) # 2
+#' 
+#' # row_count_if
+#' row_count_if(function(x) grepl("^a",x),df1) # c(1,0,0,1)
+#' 
+#' df1 %has% 'apples' # c(TRUE,FALSE,FALSE,TRUE)
+#' 
+#' # example with dplyr
+#' if (require(dplyr)){
+#'  set.seed(123)
+#'  df2 = as.data.frame(
+#'         matrix(sample(c(1:10,NA),30,replace = TRUE),10)
+#'  )
+#'  df2  %>% mutate(exact = row_count_if(8, V1, V2, V3),
+#'                     greater = row_count_if(gt(8), V1, V2, V3),
+#'                     range = row_count_if(5:8, V1, V2, V3),
+#'                     na = row_count_if(is.na, V1, V2, V3),
+#'                     not_na = row_count_if(, V1, V2, V3)
+#'                  ) -> result
+#'  result
+#' }
+sum_if=function(criterion=NULL, ..., data = NULL){
+    data = fun_if_helper(criterion = criterion, ..., data = data)
+    sum(data, na.rm = TRUE)
+}
+
+#' @export
+#' @rdname sum_if
+row_sum_if=function(criterion=NULL,..., data = NULL){
+    data = fun_if_helper(criterion = criterion, ..., data = data)
+    rowSums(data, na.rm=TRUE)
+}
+
+
+#' @export
+#' @rdname sum_if
+col_sum_if=function(criterion=NULL,..., data = NULL){
+    data = fun_if_helper(criterion = criterion, ..., data = data)
+    colSums(data, na.rm=TRUE)
+}
+
+#' @export
+#' @rdname sum_if
+mean_if=function(criterion=NULL, ..., data = NULL){
+    data = fun_if_helper(criterion = criterion, ..., data = data)
+    mean(data, na.rm = TRUE)
+}
+
+#' @export
+#' @rdname sum_if
+row_mean_if=function(criterion=NULL,..., data = NULL){
+    data = fun_if_helper(criterion = criterion, ..., data = data)
+    rowMeans(data, na.rm=TRUE)
+}
+
+
+#' @export
+#' @rdname sum_if
+col_mean_if=function(criterion=NULL,..., data = NULL){
+    data = fun_if_helper(criterion = criterion, ..., data = data)
+    colMeans(data, na.rm=TRUE)
+}
+
+
+#' @export
+fun_if_helper = function(criterion,..., data){
+    dfs = do.call(data.frame,c(list(...),stringsAsFactors=FALSE)) # form data.frame 
+    criterion = build_criterion(criterion, dfs)
+    if(is.null(data)) {
+        
+        return(set_na(dfs, !criterion))
+    }    
+
+    set_na(data) = !criterion
+    if(is.list(data) && !is.data.frame(data)){
+        data = do.call(data.frame,c(data,stringsAsFactors=FALSE))        
+    }  else {
+        if (!is.data.frame(data)){
+            data = as.data.frame(data, stringsAsFactors = FALSE)
+        } else {
+            data
+        }
+    }   
+    
+}
+
---
+++ tests/testthat/test_sum_if.R
@@ -0,0 +1,151 @@
+context("sum_if")
+df1 = data.frame(
+    a=c("apples",   "oranges",     "peaches",     "apples"),
+    b = c(32, 54, 75, 86)
+)
+
+expect_equal(sum_if("apples", df1$a, data = df1$b), 118L)
+
+
+expect_equal(sum_if(gt(55),df1$b),161L)
+
+expect_equal(sum_if(function(x) x>55 | x==32,df1$b),193L)
+expect_equal(sum_if(gt(55)|32,df1$b),193L)
+
+expect_equal(sum_if(function(x) x>55 | x==54,df1$b),215L)
+expect_equal(sum_if(gt(55)|54,df1$b),215L)
+
+expect_equal(sum_if(neq(75),df1$b),172L)
+
+expect_equal(sum_if(gte(32),df1$b),247L)
+
+expect_equal(sum_if(list(gt(32), lt(86)),df1$b),54L + 75L)
+expect_equal(sum_if(gt(32) & lt(86),df1$b),54L + 75L)
+
+expect_equal(sum_if(33:85,df1$b),54L + 75L)
+
+
+
+
+
+context("sum_if complex criteria")
+# more complex criteria
+# values with letters
+expect_equal(sum_if(function(x) grepl("^[A-z]+$",x),df1$a, data = df1$b), 247L)
+expect_error(sum_if(function(x) grepl("^[A-z]+$",x),df1, data = df1))
+
+# values that started on 'a'
+expect_equal(sum_if(function(x) grepl("^a",x), df1$a, data = df1$b),118L)
+expect_error(sum_if(eq("apples"), df1, data = df1))
+
+context("row_sum_if")
+
+
+expect_equal(row_sum_if(function(x) grepl("^a",x),df1$a, data = df1$b),c(32,0,0,86))
+
+# example with dplyr
+if(require(dplyr) && require(magrittr)){
+    set.seed(123)
+    df2 = as.data.frame(
+        matrix(sample(c(1:10,NA),30,replace = TRUE),10)
+    )
+    result = data.frame(df2,
+                        exact=c(0,8,16,0,8,8,0,0,0,0),
+                        greater=c(10,9,0,10,0,10,0,10,0,0),
+                        range=c(0,13,21,7,8,8,12,7,7,6),
+                        na=c(0,0,0,0,0,0,0,0,0,0),
+                        not_na=c(14,22,21,17,10,19,15,18,15,8)
+                        )
+    expect_equal(df2  %>% mutate(exact = row_sum_if(8,V1,V2,V3),
+                    greater = row_sum_if(gt(8),V1,V2,V3),
+                    range = row_sum_if(5:8,V1,V2,V3),
+                    na = row_sum_if(is.na,V1,V2,V3),
+                    not_na = row_sum_if(,V1,V2,V3)),
+                     result)
+}
+
+
+context("col_sum_if")
+
+
+
+
+set.seed(123)
+df2 = as.data.frame(
+    matrix(sample(c(1:10,NA),30,replace = TRUE),10)
+)
+result = data.frame(
+    exact=c(0,8,16,0,8,8,0,0,0,0),
+    greater=c(10,9,0,10,0,10,0,10,0,0),
+    range=c(0,13,21,7,8,8,12,7,7,6),
+    na=c(0,0,0,0,0,0,0,0,0,0),
+    not_na=c(14,22,21,17,10,19,15,18,15,8)
+)
+
+t_df2 = as.data.frame(t(df2))
+expect_equal(
+    with(t_df2, unname(col_sum_if(8,V1,V2,V3, V4, V5, V6, V7, V8, V9, V10))),
+    result$exact
+)
+
+expect_equal(
+    with(t_df2, unname(col_sum_if(gt(8),V1,V2,V3, V4, V5, V6, V7, V8, V9, V10))),
+    result$greater
+)
+
+expect_equal(
+    with(t_df2, unname(col_sum_if(5:8,V1,V2,V3, V4, V5, V6, V7, V8, V9, V10))),
+    result$range
+)
+
+expect_equal(
+    with(t_df2, unname(col_sum_if(is.na,V1,V2,V3, V4, V5, V6, V7, V8, V9, V10))),
+    result$na
+)
+
+expect_equal(
+    with(t_df2, unname(col_sum_if(NA,V1,V2,V3, V4, V5, V6, V7, V8, V9, V10))),
+    result$na
+)
+
+expect_equal(
+    with(t_df2, unname(col_sum_if(,V1,V2,V3, V4, V5, V6, V7, V8, V9, V10))),
+    result$not_na
+)
+
+
+#########################
+
+expect_equal(
+    with(df2, unname(col_sum_if(8,V1,V2,V3))),
+    c(0, 8, 32)
+)
+
+
+expect_equal(
+    with(df2, unname(col_sum_if(gt(8),V1,V2,V3))),
+    c(29, 10, 10)
+)
+
+expect_equal(
+    with(df2, unname(col_sum_if(5:8,V1,V2,V3))),
+    c(24, 20, 45)
+)
+
+expect_equal(
+    with(df2, unname(col_sum_if(is.na,V1,V2,V3))),
+    c(0, 0, 0)
+)
+
+expect_equal(
+    with(df2, unname(col_sum_if(NA,V1,V2,V3))),
+    c(0, 0, 0)
+)
+
+
+expect_equal(
+    with(df2, unname(col_sum_if(,V1,V2,V3))),
+    unname(colSums(df2, na.rm = TRUE))
+)
+
+    
