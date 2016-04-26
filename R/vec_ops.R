0b5341331ac6d221d7581b385bc0f5c85c4457d3
Gregory Demin <gdemin@gmail.com>
2016-04-26 14:52:47 +0300

    Add help and more tests for vector operations

--- R/vec_ops.R
+++ R/vec_ops.R
@@ -1,14 +1,64 @@
+#' Infix operations on vectors - append, diff, intersection, union, replication
+#' @param e1 vector
+#' @param e2 vector (or function for \code{\%d\%}, \code{\%i\%})
+#' 
+#' @return vector
+#' 
+#' @details 
+#' \itemize{
+#' \item{\code{\%a\%}}{ a(ppends) second argument to first argument.}
+#' \item{\code{\%u\%}}{ u(ites) first and second arguments. Remove elements from
+#' second argument that exist in first argument. }
+#' \item{\code{\%d\%}}{ d(iffs) second argument from first argument. Second
+#' argument could be a function which returns logical value. In this case
+#' elements of first argument which give TRUE will be removed }
+#' \item{\code{\%i\%}}{ i(ntersects) first argument and second argument. Second
+#' argument could be a function which returns logical value. In this case
+#' elements of first argument which give FALSE will be removed } 
+#' \item{\code{\%x\%}}{ x(or). Returns elements that contained only in one of argument.}
+#' \item{\code{\%r\%}}{ repeats first argument second argument times}
+#' } 
+#' 
+#' All these functions preserve names of vectors and doesn't remove duplicates.
+#' For \code{\%d\%} and \code{\%i\%} one can use criteria functions. See \link{count_if}
+#'  for details.
+#' 
+#' @examples 
+#' 
+#' 1:4 %a% 5:6   # 1:6
+#' 
+#' 1:4 %a% 4:5   # 1,2,3,4,4,5
+#' 
+#' 1:4 %u% 4:5   # 1,2,3,4,5
+#' 
+#' 1:6 %d% 5:6   # 1:4
+#' 
+#' 1:6 %d% gt(4) # 1:4
+#' 
+#' 1:4 %i% 4:5   # 4
+#' 
+#' letters %i% perl("[a-d]") # a,b,c,d
+#'  
+#' letters %i% (fixed("a") | fixed("z")) # a, z
+#' 
+#' 1:4 %x% 4:5   # 1, 2, 3, 5
+#' 
+#' 1:2 %r% 2     # 1, 2, 1, 2
+#'
 #' @export
 '%a%' = function(e1, e2){
     append(e1, e2)
 }
 
 #' @export
-'%m%' = function(e1, e2){
-    rep(e1, e2)
+#' @rdname grapes-a-grapes
+'%u%' = function(e1, e2){
+    c(e1, e2[!(e2 %in% e1)])
 }
 
+
 #' @export
+#' @rdname grapes-a-grapes
 '%d%' = function(e1, e2){
     if (is.function(e2)){
         e1[!e2(e1)]
@@ -18,6 +68,7 @@
 }
 
 #' @export
+#' @rdname grapes-a-grapes
 '%i%' = function(e1, e2){
     if (is.function(e2)){
         e1[e2(e1)]
@@ -26,7 +77,24 @@
     }
 }
 
+
 #' @export
+#' @rdname grapes-a-grapes
 '%x%' = function(e1, e2){
     c(e1[!(e1 %in% e2)],e2[!(e2 %in% e1)])
 }
+
+#' @export
+#' @rdname grapes-a-grapes
+'%r%' = function(e1, e2){
+    if(length(e2)==1){
+        rep(e1, e2)
+    } else {
+        stop("Multiplicator should be scalar quantity.")
+    }
+}
+
+
+
+
+
--- tests/testthat/test_vec_ops.R
+++ tests/testthat/test_vec_ops.R
@@ -8,7 +8,7 @@ expect_equal(c(a=1, b=4, d = 5) %d% lt(4), c(b=4, d=5))
 expect_equal(c(a=1, b=4, d = 5) %d% eq(4), c(a=1, d=5))
 expect_equal(c(a=1, b=4, d = 5) %d% perl(4), c(a=1, d=5))
 
-expect_equal(c(a=1, b=4, d = 5) %m% 2, c(a=1, b=4, d = 5, a=1, b=4, d = 5))
+expect_equal(c(a=1, b=4, d = 5) %r% 2, c(a=1, b=4, d = 5, a=1, b=4, d = 5))
 
 
 expect_equal(length(c(a=1, b=4) %i% c(d = 5)), 0)
@@ -16,3 +16,15 @@ expect_equal(c(a=1, b=4, f = 5) %i% c(d = 5), c(f = 5))
 
 expect_equal(c(a=1, b=4) %x% c(d = 5), c(a=1, b=4, d = 5))
 expect_equal(c(a=1, b=4, 5) %x% c(d = 5), c(a=1, b=4))
+
+
+expect_equal(1:4 %a% 5:6   , 1:6)
+expect_equal(1:4 %a% 4:5   , c(1,2,3,4,4,5))
+expect_equal(1:4 %u% 4:5   , c(1,2,3,4,5))
+expect_equal(1:6 %d% 5:6   , 1:4)
+expect_equal(1:6 %d% gt(4) , 1:4)
+expect_equal(1:4 %i% 4:5   , 4)
+expect_equal(letters %i% perl("[a-d]") , letters[1:4])
+expect_equal(letters %i% (fixed("a") | fixed("z")) , c("a", "z"))
+expect_equal(1:4 %x% 4:5   , c(1, 2, 3, 5))
+expect_equal(1:2 %r% 2     , c(1, 2, 1, 2))
