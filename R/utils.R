e5747b180cc55d899c413ef673c28da6e2bf786f
Gregory Demin <gdemin@gmail.com>
2016-05-20 01:01:15 +0300

    Simplify 'stopif' internal function

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,10 @@
 ## stop if condition with message
 
-# TODO Удалить
+# TODO Удалить export
 #' @export
 stopif = function(cond,...){
     if (isTRUE(cond)) {
-        # to know name of the parent function
-        location = deparse(sys.call(2))
-        stop(do.call(paste0,c(list(...),": ",location)),call. = FALSE)
+        stop(do.call(paste0,c(list(...))),call. = FALSE)
     }
     invisible()
 }
