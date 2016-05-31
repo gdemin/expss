48e6333b78540def5b4dfca2e656d42c31653a8b
Gregory Demin <gdemin@gmail.com>
2016-05-31 14:10:15 +0300

    Remove export of stopif

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,6 @@
 ## stop if condition with message
 
-# TODO Удалить export
-#' @export
+
 stopif = function(cond,...){
     if (isTRUE(cond)) {
         stop(do.call(paste0,c(list(...))),call. = FALSE)
