801f27792d053f900df318239e07576aa1774edb
Gregory Demin <gdemin@gmail.com>
2016-06-07 19:02:53 +0300

    Draft 'write_labels_spss'

--- R/write_labels.R
+++ R/write_labels.R
@@ -97,3 +97,72 @@ read_labelled = function(filename, colClasses = NA,fileEncoding = ""){
     
     
 }
+
+
+#' @export
+write_labels_spss = function(x, filename, fileEncoding = ""){
+    var_labs = lapply(x,var_lab)
+    val_labs = lapply(x,val_lab)
+    var_num = ncol(x)
+    x_names = colnames(x)
+    code = ""
+    identical_vallabs = vector()
+    write_labels = TRUE
+    curr_val_lab = NULL
+    
+    for (each in seq_len(var_num)){
+        
+        next_val_lab = val_labs[[each]]
+        if(identical(curr_val_lab,next_val_lab)){
+            identical_vallabs = c(identical_vallabs,x_names[each])
+        } else {
+            if(!is.null(curr_val_lab) && (curr_val_lab!="")){
+                code = paste0(code,
+                              make_make_labs_spss(identical_vallabs,curr_val_lab),           
+                              "\n\n")
+            }
+            identical_vallabs = x_names[each]
+            curr_val_lab = next_val_lab
+        }
+        
+        ##### 
+        curr_var_lab = var_labs[[each]]
+        if (!is.null(curr_var_lab) && (curr_var_lab!="")){
+            code = paste0(code,
+                          'var lab ',x_names[each],' "', gsub('"', "'", curr_var_lab),'".\n')
+            
+        }
+        
+    }
+    
+    if (length(identical_vallabs)>0){
+        code = paste0(code,
+                      make_make_labs_spss(identical_vallabs,curr_val_lab),           
+                      "\n")       
+        
+    }
+    conn = file(paste0(filename,".sps"),encoding = fileEncoding)
+    on.exit(close(conn))
+    writeLines(text= code,con = conn)
+    invisible(NULL)
+    
+}
+
+
+
+make_make_labs_spss = function(vars,named_vec){
+    if (is.null(named_vec) || is.null(names(named_vec)) || (length(vars)==0)) return(NULL)
+    sorted = order(as.numeric(named_vec))
+    named_vec = named_vec[sorted]
+    
+    if (length(vars)>1) {
+        vars = paste0("val lab ",paste(vars,collapse = " "))
+    } else {
+        vars = paste0("val lab ",vars)
+    }
+    labs = paste0('"', gsub('"',"'",names(named_vec)), '"')
+    vallab = paste0("    ",named_vec,' ',labs,'')[labs!=""]
+    vallab = paste(vallab, collapse = "\n")
+    sprintf('%s\n%s.\n',vars,vallab)
+    
+}
