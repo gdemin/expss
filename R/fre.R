9dbadc6001d71830a7b30d468599c5799e3433a3
Gregory Demin <gdemin@gmail.com>
2016-06-21 03:15:35 +0300

    Add cro, examples for fre and cro, more tests for them. No docs yet.

--- R/fre.R
+++ R/fre.R
@@ -5,10 +5,49 @@
 #' @param weight 
 #'
 #' @return data.frame
-#' @export
 #'
 #' @examples
-#' a = a + 1
+#' data(mtcars)
+#' mtcars = modify(mtcars,{
+#'     var_lab(vs) = "Engine"
+#'     val_lab(vs) = c("V-engine" = 0, 
+#'                     "Straight engine" = 1) 
+#'     var_lab(am) = "Transmission"
+#'     val_lab(am) = c(automatic = 0, 
+#'                     manual=1)
+#' })
+#' 
+#' fre(mtcars$vs)
+#' with(mtcars, cro(am, vs))
+#' with(mtcars, cro_pct(am, vs))
+#' 
+#' # multiple-choise variable
+#' # brands - multiple response question
+#' # Which brands do you use during last three months? 
+#' set.seed(123)
+#' brands = data.frame(t(replicate(20,sample(c(1:5,NA),4,replace = FALSE))))
+#' # score - evaluation of tested product
+#' score = sample(-1:1,20,replace = TRUE)
+#' var_lab(brands) = "Used brands"
+#' val_lab(brands) = make_labels("
+#'                               1 Brand A
+#'                               2 Brand B
+#'                               3 Brand C
+#'                               4 Brand D
+#'                               5 Brand E
+#'                               ")
+#' 
+#' var_lab(score) = "Evaluation of tested brand"
+#' val_lab(score) = make_labels("
+#'                              -1 Dislike it
+#'                              0 So-so
+#'                              1 Like it    
+#'                              ")
+#' 
+#' fre(brands)
+#' cro(brands, score)
+#' cro_pct(brands, score)
+#' @export
 fre = function(x, weight = NULL){
     raw = elementary_freq(x = x, weight = weight)
     not_nas = raw$not_nas
@@ -49,7 +88,11 @@ fre = function(x, weight = NULL){
     if (is.null(varlab)){
         varlab = deparse(substitute(x))
     }
+
     colnames(res) = c(varlab, "Count", "Valid percent", "Percent", "Cumulative percent")
+    if(NCOL(x)>1) {
+        res = res[, - NCOL(res), drop = FALSE]
+    }
     res
     
 }
@@ -59,6 +102,9 @@ fre = function(x, weight = NULL){
 elementary_freq = function(x, predictor = NULL, weight = NULL){
     stopif(!is.null(weight) && (NROW(x)!=length(weight)), "weight should have the same number of rows as x.")
     stopif(NCOL(predictor)>1, "predictor should have only one column.")
+    if (is.matrix(x)) {
+        x = as.data.frame(x, stringsAsFactors = FALSE, check.names = FALSE)
+    }
     if (is.null(weight)) {
         weight = rep(1, NROW(x))
     } else {
@@ -115,51 +161,56 @@ elementary_freq = function(x, predictor = NULL, weight = NULL){
 #' @export
 #' @rdname fre
 cro = function(x, predictor, weight = NULL){
+    stopif(NROW(x)!=length(predictor), "predictor should have the same number of rows as x.")
     raw = elementary_freq(x = x, predictor = predictor, weight = weight)
-    column_total = raw
-    not_nas = raw$not_nas
-    nas = raw$nas
     res = raw$freq
-    res = res[!is.na(res$res), ]
     
-    if(not_nas>0) {
-        res$valid_percent =  res$res/not_nas*100    
-    } else {
-        res$valid_percent = 0
-    }    
-    base = not_nas + nas
-    if(base>0) {
-        res$percent =  res[[2]]/base*100    
-    } else {
-        res$percent = 0
-    }   
-    total = sum_col(res[,-1])
-    dfs_total = data.frame(
-        labels = "#Total",
-        res = total[1],
-        valid_percent = total[2], 
-        percent = total[3],
-        cum = NA
+    
+    column_total = if_na(raw$total, 0) 
+    res = res[, c(TRUE, column_total>0), drop = FALSE]
+    column_total = column_total[column_total>0]
+
+    column_total = data.frame(labels = "#Total(counts)", 
+                              t(column_total), 
+                              stringsAsFactors = FALSE,
+                              check.names = FALSE
     )
-    res$cum = cumsum(res$valid_percent)
-    dfs_na = data.frame(labels = "<NA>", 
-                        res = nas, 
-                        valid_percent = NA, 
-                        percent = ifelse(base>0, nas/base*100, 0),
-                        cum = NA
-    )
-    res = rbind(res, dfs_total, dfs_na)
-    rownames(res) = NULL
+    res = rbind(res, column_total)
+    row_total = sum_row(res[,-1])
+    res[,"#row_total"] = row_total
+    res = res[row_total>0,, drop = FALSE]
     
     varlab = var_lab(x)
     if (is.null(varlab)){
         varlab = deparse(substitute(x))
     }
-    colnames(res) = c(varlab, "Count", "Valid percent", "Percent", "Cumulative percent")
+    
+    colnames(res)[1] = varlab
+    colnames(res)[NCOL(res)] = "#Total"
     res
     
 }
 
-
+#' @export
+#' @rdname fre
+cro_pct = function(x, predictor, weight = NULL){
+    res = cro(x = x, predictor = predictor, weight = weight)
+    last_row = NROW(res)
+    if(NCOL(res)>1 & last_row>1){
+        total_row = res[last_row, ]
+        for (i in seq_along(res)[-1]){
+            res[[i]][-last_row] = res[[i]][-last_row]/total_row[[i]]*100 
+        }    
+        
+    }
+    varlab = var_lab(x)
+    if (is.null(varlab)){
+        varlab = deparse(substitute(x))
+    }
+    colnames(res)[1] = varlab
+    res
+    
+    
+}
 
 
--- tests/testthat/test_fre.R
+++ tests/testthat/test_fre.R
@@ -164,4 +164,78 @@ context("elementary_freq factor")
 expect_equal_to_reference(expss:::elementary_freq(iris$Species), "rds/fre19.rds")
 expect_equal_to_reference(expss:::elementary_freq(iris$Species, iris$Species), "rds/fre20.rds")
 
+context("cro count")
+
+data(mtcars)
+mtcars = modify(mtcars,{
+    var_lab(mpg) = "Miles/(US) gallon"
+    var_lab(cyl) = "Number of cylinders"
+    var_lab(disp) = "Displacement (cu.in.)"
+    var_lab(hp) = "Gross horsepower"
+    var_lab(drat) = "Rear axle ratio"
+    var_lab(wt) = "Weight (lb/1000)"
+    var_lab(qsec) = "1/4 mile time"
+    var_lab(vs) = "Engine"
+    val_lab(vs) = c("V-engine" = 0, 
+                    "Straight engine" = 1) 
+    var_lab(am) = "Transmission"
+    val_lab(am) = c(automatic = 0, 
+                    manual=1)
+    var_lab(gear) = "Number of forward gears"
+    var_lab(carb) = "Number of carburetors"
+})
+
+with(mtcars, cro(am, vs))
+
+context("fre and cro examples")
+
+
+data(mtcars)
+mtcars = modify(mtcars,{
+    var_lab(vs) = "Engine"
+    val_lab(vs) = c("V-engine" = 0, 
+                    "Straight engine" = 1) 
+    var_lab(am) = "Transmission"
+    val_lab(am) = c(automatic = 0, 
+                    manual=1)
+})
+
+expect_equal_to_reference(fre(mtcars$vs), "rds/fre_ex1.rds")
+expect_equal_to_reference(with(mtcars, cro(am, vs)), "rds/fre_ex2.rds")
+expect_equal_to_reference(with(mtcars, cro_pct(am, vs)), "rds/fre_ex3.rds")
+
+# multiple-choise variable
+# brands - multiple response question
+# Which brands do you use during last three months? 
+set.seed(123)
+brands = data.frame(t(replicate(20,sample(c(1:5,NA),4,replace = FALSE))))
+# score - evaluation of tested product
+score = sample(-1:1,20,replace = TRUE)
+var_lab(brands) = "Used brands"
+val_lab(brands) = make_labels("
+                              1 Brand A
+                              2 Brand B
+                              3 Brand C
+                              4 Brand D
+                              5 Brand E
+                              ")
+
+var_lab(score) = "Evaluation of tested brand"
+val_lab(score) = make_labels("
+                             -1 Dislike it
+                             0 So-so
+                             1 Like it    
+                             ")
+
+expect_equal_to_reference(fre(brands), "rds/fre_ex4.rds")
+expect_equal_to_reference(cro(brands, score), "rds/fre_ex5.rds")
+expect_equal_to_reference(cro_pct(brands, score), "rds/fre_ex6.rds")
+
+
+
+
+
+
+
+
 
