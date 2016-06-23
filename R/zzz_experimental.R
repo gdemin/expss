bf66f0b8f61a07152a2377e4e5c7a6c900758f0a
Gregory Demin <gdemin@gmail.com>
2016-06-23 20:20:01 +0300

    Add functions for operations on default dataset, help and tests for them.

--- R/zzz_experimental.R
+++ R/zzz_experimental.R
@@ -1,14 +1,117 @@
-#' Experimental functions for working with default dataset
+#' Experimental functions for operations with default dataset
 #'
-#'
-#'
-#' @param x dsfs
-#' @param expr dfsdf
-#' @param cond sdfsdf
-#' @param ... dsfsdf
+#' Workflow for this functions is rather simple. You should set up default 
+#' data.frame with \link{default_dataset} and then operate with it without any 
+#' reference to your data.frame. There are to kind of operations. The first kind
+#' modify default dataset - their names start with double dots. Functions of the
+#' second kind will be evaluated in the context of the default dataset but 
+#' doesn't modify it. Their names start with single dot. It is not recommended 
+#' to use one of these functions in the scope of another of these functions. By 
+#' now their performance is not so high, especially
+#' \code{..do_if}/\code{..modify_if} can be very slow.
+#' 
+#' @details 
+#' Double dot functions:
+#' \itemize{
+#' \item{\code{..modify}}{ Add and modify variables inside default data.frame. See
+#' \link{modify}.}
+#' \item{\code{..compute}}{ Shortcut for \code{..modify}. Name is inspired by
+#' SPSS COMPUTE operator. See \link{modify}.}
+#' \item{\code{..modify_if}}{ Add and modify variables inside subset of default
+#' data.frame. See \link{modify_if}.}
+#' \item{\code{..do_if}}{ Shortcut for \code{..modify_if}. Name is inspired by
+#' SPSS DO IF operator. See \link{modify_if}.}
+#' \item{\code{..filter}}{ Leave subset of default data.frame which meet
+#' condition. See \link[base]{subset}.}
+#' \item{\code{..set_var_lab}}{ Set variable label in the default data.frame. See
+#' \link{set_var_lab}.}
+#' \item{\code{..set_val_lab}}{ Set value labels for variable in the default
+#' data.frame. See \link{set_val_lab}.}
+#' \item{\code{..add_val_lab}}{ Add value labels for variable in the default
+#' data.frame. See \link{add_val_lab}.}
+#' \item{\code{..if_val}}{ Change, rearrange or consolidate the values of an existing
+#' variable inside default data.frame. See \link{if_val}.}
+#' \item{\code{..recode}}{ Shortcut for \code{..if_val}. Name is inspired by
+#' SPSS RECODE. See \link{if_val}.}
+#' }
+#' Single dot functions:
+#' \itemize{
+#' \item{\code{.var_lab}}{ Return variable label from default dataset. See \link{var_lab}.}
+#' \item{\code{.val_lab}}{ Return value labels from default dataset. See \link{val_lab}.}
+#' \item{\code{.fre }}{ Simple frequencies of variable in the default
+#' data.frame.  See \link{fre}.}
+#' \item{\code{.cro}/\code{.cro_cpct}/\code{.cro_rpct}/\code{.cro_tpct}}{ Simple
+#' crosstabulations of variable in the default data.frame.  See \link{cro}.}
+#' \item{\code{.with}}{ Evaluate arbitrary expression in the context of
+#' data.frame.  See \link[base]{with}.}
+#' }
+#' @param x vector/data.frame - variable names in the scope of default dataset
+#' @param expr set of expressions  in curly brackets which will be evaluated in
+#'   the context of default dataset
+#' @param cond logical vector/expression
+#' @param ... further arguments 
 #'
 #' @examples 
-#' a = 1
+#' data(mtcars)
+#' 
+#' default_dataset(mtcars) # set mtcars as default dataset
+#' 
+#' # calculate new variables
+#' ..compute({
+#'     mpg_by_am = ave(mpg, am, FUN = mean)
+#'     hi_low_mpg = ifs(mpg<mean(mpg) ~ 0, other = 1)    
+#' })
+#' 
+#' # set labels
+#' ..set_var_lab(mpg_by_am, "Average mpg for transimission type")
+#' ..set_var_lab(hi_low_mpg, "Miles per gallon")
+#' ..set_val_lab(hi_low_mpg, ml_left("
+#'                                   0 Low
+#'                                   1 High
+#'                                   "))
+#' 
+#' ..set_var_lab(vs, "Engine")
+#' ..set_val_lab(vs, ml_left(" 
+#'                           0 V-engine
+#'                           1 Straight engine
+#'                           "))
+#' 
+#' ..set_var_lab(am, "Transmission")
+#' ..set_val_lab(am, ml_left(" 
+#'                           0 automatic
+#'                           1 manual
+#'                           "))
+#' 
+#' # calculate frequencies
+#' .fre(hi_low_mpg)
+#' .cro(mpg_by_am, am)
+#' .cro(cyl, hi_low_mpg)
+#' 
+#' # disable default dataset
+#' default_dataset(NULL)
+#' 
+#' # Example of ..recode
+#' 
+#' data(iris)
+#' 
+#' default_dataset(iris) # set iris as default dataset
+#' 
+#' ..recode(Sepal.Length, lo %thru% median(Sepal.Length) ~ "small", . ~ "large")
+#' 
+#' head(iris)
+#' .fre(Sepal.Length)
+#' 
+#' # example of ..do_if
+#'  
+#' ..do_if(Species == "setosa",{
+#'      Petal.Length = NA
+#'      Petal.Width = NA
+#' })
+#' 
+#' .cro(Petal.Width, Species)
+#' 
+#' # disable default dataset
+#' default_dataset(NULL)
 #' @export
 #' @name compute
 ..modify = function (expr) {
@@ -72,11 +175,12 @@ modify_default_dataset_light = function(x, ...){
     parent = parent.frame()
     e = evalq(environment(), data, parent)
     e$.n = nrow(data)
-    if (length(all.vars(for_names, functions = TRUE))==1){
+    if (length(all.vars(for_names, functions = FALSE))==1 & length(all.vars(for_names, functions = TRUE))==1){
         for_names = as.character(for_names) 
     } else {
         for_names = names(eval(for_names, e))
     }
+    stopif(length(for_names)==0, "Something is going wrong. Variables not found: ", deparse((substitute(x))))
     res = eval(expr, e)
     data[, for_names] = res
     ref(reference) = data
@@ -105,6 +209,46 @@ eval_in_default_dataset = function(...){
 #' @rdname compute
 ..compute = ..modify
 
+#' @export
+#' @rdname compute
+..filter = function (cond) {
+    # based on 'within' from base R by R Core team
+    reference = suppressMessages(default_dataset() )
+    data = ref(reference)
+    parent = parent.frame()
+    cond = substitute(cond)
+    cond = eval(cond, data, parent.frame())
+    if (!is.logical(cond)) 
+        stop("'cond' must be logical")
+    cond = cond & !is.na(cond)
+    new_data = data[cond,, drop = FALSE]
+    ref(reference) = new_data
+    invisible(NULL)
+}
+
+# #' @export
+# #' @rdname compute
+# ..select = function (...) {
+#     # based on 'within' from base R by R Core team
+#     reference = suppressMessages(default_dataset())
+#     data = ref(reference)
+#     parent = parent.frame()
+#     e = evalq(environment(), data, parent)
+#     res = eval(substitute(list(...)), e)
+# 
+#     ref(reference) = as.data.frame(res, stringsAsFactors = FALSE, check.names = FALSE)
+#     invisible(NULL)
+# }
+
+
+
+#' @export
+#' @rdname compute
+.with = function (expr, ...) {
+    reference = suppressMessages(default_dataset() )
+    data = ref(reference)
+    eval(substitute(expr), data, enclos = parent.frame())
+}    
 
 #' @export
 #' @rdname compute
@@ -143,11 +287,12 @@ eval_in_default_dataset = function(...){
     parent = parent.frame()
     e = evalq(environment(), data, parent)
     e$.n = nrow(data)
-    if (length(all.vars(for_names, functions = TRUE))==1){
+    if (length(all.vars(for_names, functions = FALSE))==1 & length(all.vars(for_names, functions = TRUE))==1){
         for_names = as.character(for_names) 
     } else {
         for_names = names(eval(for_names, e))
     }
+    stopif(length(for_names)==0, "Something is going wrong. Variables not found: ", deparse((substitute(x))))
     res = eval(expr, e)
     data[, for_names] = res
     ref(reference) = data
--- tests/testthat/test_experimental.R
+++ tests/testthat/test_experimental.R
@@ -24,3 +24,99 @@ expect_equal_to_reference(.cro_tpct(q8r_1 %to% q8r_99, reg), "rds/cro_real4t.rds
 expect_equal_to_reference(.fre(reg, weight = weight1), "rds/fre_real1w.rds")
 expect_equal_to_reference(.cro_rpct(q8r_1 %to% q8r_99, s1, weight = weight1), "rds/cro_real6wr.rds")
 
+
+data(iris)
+default_iris = iris
+default_dataset(default_iris)
+
+expect_error(..recode(colnames(iris), . ~ tolower))
+
+..filter(Species == "setosa")
+expect_identical(default_iris, iris[iris$Species == "setosa", ])
+
+
+data("mtcars")
+
+default_mtcars = mtcars
+default_dataset(default_mtcars)
+
+..compute({
+    mpg_by_am = ave(mpg, am, FUN = mean)
+    hi_low_mpg = ifs(mpg<mean(mpg) ~ 0, mpg>mean(mpg) ~ 1)
+    var_lab(hi_low_mpg) = "Miles per gallon"
+    val_lab(hi_low_mpg) = ml_left("
+                                 0 Low
+                                 1 High
+                                 ")
+})
+
+..set_var_lab(vs, "Engine")
+..set_val_lab(vs, c("V-engine" = 0)) 
+..add_val_lab(vs, c("Straight engine" = 1))
+
+..set_var_lab(am, "Transmission")
+..set_val_lab(am, c(automatic = 0,  manual=1))
+
+mtcars = within(mtcars,{
+    mpg_by_am = ave(mpg, am, FUN = mean)
+    hi_low_mpg = ifs(mpg<mean(mpg) ~ 0, mpg>mean(mpg) ~ 1)
+    var_lab(hi_low_mpg) = "Miles per gallon"
+    val_lab(hi_low_mpg) = ml_left("
+                                 0 Low
+                                 1 High
+                                 ")
+    
+    vs = set_var_lab(vs, "Engine")
+    vs = set_val_lab(vs, c("V-engine" = 0)) 
+    vs = add_val_lab(vs, c("Straight engine" = 1))
+    
+    am = set_var_lab(am, "Transmission")
+    am = set_val_lab(am, c(automatic = 0,  manual=1))
+    
+})
+
+nn = ncol(mtcars)
+
+expect_identical(mtcars[, c(1:(nn-2), nn, nn-1)], default_mtcars)
+expect_identical(.fre(vs),fre(mtcars$vs))
+expect_identical(.with(fre(vs)),fre(mtcars$vs))
+expect_identical(.cro(am, vs), cro(mtcars$am, mtcars$vs))
+expect_identical(.cro_cpct(am, vs), cro_cpct(mtcars$am, mtcars$vs))
+var_lab(mtcars$mpg_by_am) = "mpg_by_am"
+expect_identical(.cro_cpct(mpg_by_am, hi_low_mpg), cro_cpct(mtcars$mpg_by_am, mtcars$hi_low_mpg))
+
+############
+rm(mtcars)
+data("mtcars")
+
+default_mtcars = mtcars
+default_dataset(default_mtcars)
+
+..do_if(vs == 0, {
+    mean_mpg = mean(mpg)
+    vs_0 = 1
+    mpg_by_am = ave(mpg, am, FUN = mean)
+    hi_low_mpg = ifs(mpg<mean(mpg) ~ 0, other = 1)
+})
+
+
+mtcars = within(mtcars,{
+    hi_low_mpg = NA
+    hi_low_mpg[vs==0] = ifelse(mpg[vs==0]<mean(mpg[vs==0]), 0,1)
+    mpg_by_am = NA
+    mpg_by_am[vs==0] = ave(mpg[vs==0], am[vs==0], FUN = mean)
+    vs_0 = ifelse(vs == 0, 1, NA)
+    mean_mpg = NA
+    mean_mpg[vs == 0] = mean(mpg[vs==0])
+})
+
+
+
+expect_identical(mtcars, default_mtcars)
+
+
+
+
+
+
+
