b728a2d561ace1cc55ee4f9c2e852f59b648940f
Gregory Demin <gdemin@gmail.com>
2016-07-01 00:51:09 +0300

    Stricter behavior in 'stopif'

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 ## stop if condition with message
 stopif = function(cond,...){
-    if (isTRUE(cond)) {
+    if (cond) {
         stop(do.call(paste0,c(list(...))),call. = FALSE)
     }
     invisible()
