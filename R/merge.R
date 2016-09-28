4c3d6ee6a088ad8a7d03f308e983c20d1758ab1a
Gregory Demin <gdemin@gmail.com>
2016-09-29 02:20:27 +0300

    Add %merge%

---
+++ R/merge.R
@@ -0,0 +1,105 @@
+#' Merge two tables/data.frames
+#'
+#' \code{\%merge\%} is infix shortcut for base \link[base]{merge} with 
+#' \code{all.x = TRUE} and  \code{all.y = FALSE} (left join). There is also 
+#' special method for combining results of \code{cro_*} and \code{fre}. For them
+#' \code{all = TRUE} (full join). It allows make complex tables from simple
+#' ones. See examples. Strange result is possible if one or two arguments have
+#' duplicates in first column (column with labels).
+#'
+#' @seealso \link{fre}, \link{cro}, \link[base]{merge}
+#'
+#' @param x data.frame or results of \code{fre}/\code{cro_*}
+#' @param y data.frame or results of \code{fre}/\code{cro_*}
+#'
+#' @return data.frame
+#' @name merge.simple_table
+#' @export
+#'
+#' @examples
+#' data(mtcars)
+#' # apply labels
+#' mtcars = modify(mtcars,{
+#'                 var_lab(mpg) = "Miles/(US) gallon"
+#'                 var_lab(cyl) = "Number of cylinders"
+#'                 var_lab(disp) = "Displacement (cu.in.)"
+#'                 var_lab(hp) = "Gross horsepower"
+#'                 var_lab(drat) = "Rear axle ratio"
+#'                 var_lab(wt) = "Weight (lb/1000)"
+#'                 var_lab(qsec) = "1/4 mile time"
+#'                 var_lab(vs) = "V/S"
+#'                 val_lab(vs) = c("V-engine" = 0, "Straight engine" = 1)
+#'                 var_lab(am) = "Transmission (0 = automatic, 1 = manual)"
+#'                 val_lab(am) = c(automatic = 0, manual = 1)
+#'                 var_lab(gear) = "Number of forward gears"
+#'                 var_lab(carb) = "Number of carburetors"
+#' })
+#'
+#' # table by 'am'
+#' tab1 = with(mtcars, cro_cpct(gear, am))
+#' # table with percents
+#' tab2 = with(mtcars, cro_cpct(gear, vs))
+#'
+#' # combine tables
+#' # %n_d% remove first total
+#' tab1 %n_d% "#Total" %merge% tab2
+'%merge%' = function(x, y) UseMethod('%merge%')
+
+#' @export
+'%merge%.default' = function(x, y) merge(x, y, all.x = TRUE, all.y = FALSE)
+
+#' @export
+'%merge%.simple_table' = function(x, y) merge.simple_table(x, y)
+
+
+#' @export
+merge.simple_table = function(x, y,
+                        by.x = colnames(x)[1],
+                        by.y = colnames(y)[1],
+                        all = TRUE,
+                        all.x = all,
+                        all.y = all,
+                        sort = FALSE,
+                        suffixes = c("",""),
+                        incomparables = NULL, ...){
+
+    class_x = class(x)
+    class_y = class(y)
+    # below we try to preserve order in rows in y for rows which doesn't exists in x
+    order.x = seq_len(nrow(x))
+    x[['..order..x']] = order.x
+    order.y = order.x[match(y[[by.y]], x[[by.x]])]
+    # fill NA.
+    need_sort = anyNA(order.y) & !all(is.na(order.y))
+    if(need_sort){
+        delta = 1/length(order.y)/10
+        if(is.na(order.y[1])) order.y[1] = 0
+        for (i in seq_along(order.y)[-1]){
+            if(is.na(order.y[i])){
+                order.y[i] = order.y[i-1] + delta
+            }
+        }
+        y[['..order..y']] = order.y
+    }
+    
+    res = suppressWarnings(merge.data.frame(x, y, by.x = by.x, by.y = by.y,
+                                            all.x = all.x, all.y = all.y,
+                                            sort = sort, suffixes = suffixes,
+                                            incomparables = incomparables,
+                                            ... ))
+    preserve_colnames = colnames(res) %d% c('..order..y','..order..x')
+    if(need_sort){
+        order.y = res[['..order..y']]
+        if_na(order.y) = res[['..order..x']]
+        res = res[order(order.y), , drop = FALSE]
+    } else {
+        res = res[order(res[['..order..x']]), , drop = FALSE]
+        
+    }
+    res = res[, colnames(res) %d% c('..order..y','..order..x'), drop = FALSE]
+    colnames(res) = preserve_colnames
+    class(res) = intersect(class_x, class_y)
+    if(!("simple_table" %in% class(res))) class(res) = c("simple_table", class(res))
+    res
+
+}
---
+++ tests/testthat/test_merge.R
@@ -0,0 +1,64 @@
+context("merge")
+
+data("mtcars")
+
+mtcars = modify(mtcars, {
+    var_lab(vs) = "vs"
+    var_lab(am) = "am"
+    val_lab(am) = c("automatic transmission" = 1, "manual transmission" = 0)
+    var_lab(gear) = "gear"
+    var_lab(carb) = "carb"})
+
+
+### need tests for simple_table - fre and cro
+
+context("merge simple_table")
+expect_equal_to_reference(with(mtcars,{
+    freq = fre(carb)
+    cross = cro(carb, am)
+    freq %merge% cross
+
+
+}), "rds/merge7.rds")
+
+expect_equal_to_reference(with(mtcars,{
+    freq = fre(carb)
+    cross = cro(carb, am)
+    cross %merge% freq
+
+
+}), "rds/merge8.rds")
+
+expect_equal_to_reference(with(mtcars,{
+    freq = fre(carb[carb!=2])
+    cross = cro(carb[carb!=4], am[carb!=4])
+    freq %merge% cross
+
+
+}), "rds/merge9.rds")
+
+# weird
+expect_equal_to_reference(with(mtcars,{
+    freq = fre(carb)
+    cross = cro(vs, am)
+    cross %merge% freq
+
+
+}), "rds/merge10.rds")
+
+expect_equal_to_reference(with(mtcars,{
+    freq1 = fre(gear)
+    freq2 = fre(carb)
+    merge(freq1, freq2, suffixes = c(" gear", " carb"))
+}), "rds/merge11.rds")
+
+
+context("left join")
+
+a = dtfrm(x = 1:5, y = 11:15)
+b = dtfrm(x = 3:7, z = 13:17)
+expect_identical(a %merge% b, merge(a, b, all.x = TRUE, all.y = FALSE))
+
+
+
+
