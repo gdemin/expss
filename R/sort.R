b28407accdc0c81c3e63b181892b99ca773fbd03
Gregory Demin <gdemin@gmail.com>
2016-10-10 04:01:11 +0300

    Add 'sort_asc'/'sort_desc'

---
+++ R/sort.R
@@ -0,0 +1,154 @@
+#' Sort data.frames/matrices/vectors
+#' 
+#' \code{sort_asc} sorts in ascending order and \code{sort_desc} sorts in 
+#' descending order. There is no non-standard evaluation in these functions by 
+#' design so use quotes for names of your variables or use \link{qc}.
+#' \code{\%sort_asc\%}/\code{\%sort_desc\%} are infix versions of these functions. 
+#' \code{.sort_asc}/\code{.sort_desc} are versions for working with
+#' \link{default_dataset}.
+#'
+#' @param data data.frame/matrix/vector
+#' @param ... character/numeric. Column names/numbers for data.frame/matrix by
+#'   which object will be sorted. Ignored for vectors.
+#' @param variables character/numeric. Column names/numbers for data.frame/matrix by
+#'   which object will be sorted for infix functions. Ignored for vectors.
+#' @param na.last for controlling the treatment of NAs. If TRUE, missing values
+#'   in the data are put last; if FALSE, they are put first; if NA, they are
+#'   removed.
+#'
+#' @return sorted \code{data}
+#' @export
+#'
+#' @examples
+#' data(mtcars)
+#' sort_asc(mtcars, "mpg")
+#' sort_asc(mtcars, "cyl", "mpg") # by two column
+#' 
+#' # same results with column nums
+#' sort_asc(mtcars, 1)
+#' sort_asc(mtcars, 2:1) # by two column
+#' sort_asc(mtcars, 2, 1) # by two column
+#' 
+#' # 'qc'  usage
+#' sort_asc(mtcars, qc(cyl, mpg)) 
+#' 
+#' # infix version
+#' mtcars %sort_asc% "mpg"
+#' mtcars %sort_asc% c("cyl", "mpg")
+#' mtcars %sort_asc% qc(cyl, mpg) 
+#' 
+sort_asc = function(data, ..., na.last = FALSE){
+    UseMethod("sort_asc")
+}
+
+#' @export
+sort_asc.default = function(data, ..., na.last = FALSE){
+    sort(data, decreasing = FALSE, na.last = na.last, ...)
+}
+
+#' @export
+sort_asc.data.frame = function(data, ..., na.last = FALSE){
+    sort_internal(data, ..., decreasing = FALSE, na.last = na.last)
+}
+
+#' @export
+sort_asc.matrix = function(data, ..., na.last = FALSE){
+    sort_internal(data, ..., decreasing = FALSE, na.last = na.last)
+}
+
+
+sort_internal = function(data, ..., decreasing, na.last){
+    args = list(...)
+    if(any(sapply(args, is.list))){
+        args = unlist(args, recursive = FALSE)
+    }
+    numeric_args = args 
+    colnum = ncol(data)    
+    varnames = colnames(data)
+    for(each in seq_along(args)){
+        curr = args[[each]]
+        if(is.character(curr) & length(curr)>0){
+            stopif(!all(curr %in% varnames), "some column names don't exist in 'data': ", curr %d% varnames)
+            numeric_args[[each]] = match(curr, varnames)        
+        }
+    }
+    numeric_args = unlist(numeric_args)
+    
+    stopif(max(numeric_args, na.rm = TRUE) > colnum,
+        "some column numbers are larger than number of columns in 'data': ", numeric_args[numeric_args>colnum])
+    if(is.matrix(data)){
+        columns_list = lapply(numeric_args, function(each_col) data[, each_col]) 
+    } else {
+        columns_list = lapply(numeric_args, function(each_col) data[[each_col]])   
+    }
+    new_order = do.call(order, c(columns_list, decreasing = decreasing, na.last = na.last))
+    data[new_order, , drop = FALSE]    
+}
+
+#' @export
+sort_asc.list= function(data, ..., na.last = FALSE){
+    stop("Sorting in not implemented for lists.")
+}
+
+#' @rdname sort_asc
+#' @export
+'%sort_asc%' = function(data, variables){
+    sort_asc(data, variables)
+}
+
+
+#' @rdname sort_asc
+#' @export
+.sort_asc = function(..., na.last = FALSE){
+    reference = suppressMessages(default_dataset() )
+    data = ref(reference)
+    ref(reference) = sort_asc(data, ..., na.last = na.last)
+    invisible(NULL)
+}
+######### sort_desc ##############
+
+#' @rdname sort_asc
+#' @export
+sort_desc = function(data, ..., na.last = TRUE){
+    UseMethod("sort_desc")
+}
+
+#' @rdname sort_asc
+#' @export
+.sort_desc = function(..., na.last = TRUE){
+    reference = suppressMessages(default_dataset() )
+    data = ref(reference)
+    ref(reference) = sort_desc(data, ..., na.last = na.last)
+    invisible(NULL)
+}
+
+#' @export
+sort_desc.default = function(data, ..., na.last = TRUE){
+    sort(data, decreasing = TRUE, na.last = na.last, ...)
+}
+
+#' @export
+sort_desc.data.frame = function(data, ..., na.last = TRUE){
+    sort_internal(data, ..., decreasing = TRUE, na.last = na.last)
+}
+
+#' @export
+sort_desc.matrix = function(data, ..., na.last = TRUE){
+    sort_internal(data, ..., decreasing = TRUE, na.last = na.last)
+}
+
+#' @export
+sort_desc.list= function(data, ..., na.last = TRUE){
+    stop("Sorting in not implemented for lists.")
+}
+
+#' @rdname sort_asc
+#' @export
+'%sort_desc%' = function(data, variables){
+    sort_desc(data, variables)
+}
+
+
+
+
+
---
+++ tests/testthat/test_sort.R
@@ -0,0 +1,103 @@
+context("sort vector")
+
+set.seed(123)
+
+a = sample(c(1:10, NA, NA, NA))
+
+expect_identical(sort_asc(a), sort(a, decreasing = FALSE, na.last = FALSE))
+expect_identical(sort_desc(a), sort(a, decreasing = TRUE, na.last = TRUE))
+
+expect_identical(sort_asc(a, na.last = TRUE), sort(a, decreasing = FALSE, na.last = TRUE))
+expect_identical(sort_desc(a, na.last = FALSE), sort(a, decreasing = TRUE, na.last = FALSE))
+
+expect_identical(sort_asc(a, na.last = NA), sort(a, decreasing = FALSE, na.last = NA))
+expect_identical(sort_desc(a, na.last = NA), sort(a, decreasing = TRUE, na.last = NA))
+
+context("sort data.frame")
+data(iris)
+expect_identical(sort_asc(iris, "Sepal.Length"), iris[order(iris$Sepal.Length, decreasing = FALSE), ])
+expect_identical(sort_asc(iris, "Sepal.Length", 2), iris[order(iris$Sepal.Length, iris$Sepal.Width, decreasing = FALSE), ])
+expect_identical(sort_asc(iris, qc(Sepal.Length, Sepal.Width), 3:4), 
+                 iris[order(iris$Sepal.Length, iris$Sepal.Width, iris$Petal.Length, iris$Petal.Width, decreasing = FALSE), ])
+
+expect_identical(sort_asc(iris, 3:4, qc(Sepal.Length, Sepal.Width)), 
+                 iris[order(iris$Petal.Length, iris$Petal.Width, iris$Sepal.Length, iris$Sepal.Width,  decreasing = FALSE), ])
+
+
+expect_identical(sort_desc(iris, "Sepal.Length"), iris[order(iris$Sepal.Length, decreasing = TRUE), ])
+expect_identical(sort_desc(iris, "Sepal.Length", 2), iris[order(iris$Sepal.Length, iris$Sepal.Width, decreasing = TRUE), ])
+expect_identical(sort_desc(iris, qc(Sepal.Length, Sepal.Width), 3:4), 
+                 iris[order(iris$Sepal.Length, iris$Sepal.Width, iris$Petal.Length, iris$Petal.Width, decreasing = TRUE), ])
+
+expect_identical(sort_desc(iris, 3:4, qc(Sepal.Length, Sepal.Width)), 
+                 iris[order(iris$Petal.Length, iris$Petal.Width, iris$Sepal.Length, iris$Sepal.Width,  decreasing = TRUE), ])
+
+single_col = iris[,"Sepal.Length", drop = FALSE]
+expect_identical(sort_asc(single_col, "Sepal.Length"), 
+                 single_col[order(single_col$Sepal.Length, decreasing = FALSE), , drop = FALSE])
+expect_identical(sort_desc(single_col, "Sepal.Length"), 
+                 single_col[order(single_col$Sepal.Length, decreasing = TRUE), , drop = FALSE])
+
+expect_identical(iris %sort_desc% "Sepal.Length", iris[order(iris$Sepal.Length, decreasing = TRUE), ])
+expect_identical(iris %sort_desc% "Species", iris[order(iris$Species, decreasing = TRUE), ])
+expect_identical(iris %sort_desc% list("Sepal.Length", 2), iris[order(iris$Sepal.Length, iris$Sepal.Width, decreasing = TRUE), ])
+expect_identical(iris %sort_asc% list("Sepal.Length", 2), iris[order(iris$Sepal.Length, iris$Sepal.Width, decreasing = FALSE), ])
+
+data(mtcars)
+expect_identical(sort_asc(mtcars, 2:1), sort_asc(mtcars, "cyl", "mpg"))
+
+iris[1:5,1] = NA
+
+expect_identical(sort_asc(iris, "Sepal.Length"), iris[order(iris$Sepal.Length, decreasing = FALSE, na.last = FALSE), ])
+expect_identical(sort_desc(iris, "Sepal.Length"), iris[order(iris$Sepal.Length, decreasing = TRUE, na.last = TRUE), ])
+
+expect_identical(sort_asc(iris, "Sepal.Length", na.last = TRUE), 
+                 iris[order(iris$Sepal.Length, decreasing = FALSE, na.last = TRUE), ])
+expect_identical(sort_desc(iris, "Sepal.Length", na.last = FALSE), 
+                 iris[order(iris$Sepal.Length, decreasing = TRUE, na.last = FALSE), ])
+
+def = iris
+default_dataset(def)
+
+.sort_asc("Sepal.Length")
+expect_identical(def, iris[order(iris$Sepal.Length, decreasing = FALSE, na.last = FALSE), ])
+
+.sort_desc("Sepal.Length")
+expect_identical(def, iris[order(iris$Sepal.Length, decreasing = TRUE, na.last = TRUE), ])
+
+.sort_asc("Sepal.Length", na.last = TRUE)
+expect_identical(def, 
+                 iris[order(iris$Sepal.Length, decreasing = FALSE, na.last = TRUE), ])
+
+.sort_desc("Sepal.Length", na.last = FALSE)
+expect_identical(def, 
+                 iris[order(iris$Sepal.Length, decreasing = TRUE, na.last = FALSE), ])
+
+
+expect_error(sort_asc(iris, "not_exists"))
+expect_error(sort_asc(iris, 12))
+
+expect_error(sort_asc(as.list(1:3)))
+expect_error(sort_desc(as.list(1:3)))
+
+context("sort matrices")
+set.seed(123)
+mat = matrix(sample(1:4, 18, replace = TRUE), ncol = 3)
+
+expect_identical(sort_asc(mat, 1), mat[order(mat[,1], decreasing = FALSE), ])
+expect_identical(sort_asc(mat, 1:2), mat[order(mat[,1], mat[,2], decreasing = FALSE), ])
+expect_identical(sort_asc(mat, 1,2), mat[order(mat[,1], mat[,2], decreasing = FALSE), ])
+
+expect_identical(sort_desc(mat, 1), mat[order(mat[,1], decreasing = TRUE), ])
+expect_identical(sort_desc(mat, 1:2), mat[order(mat[,1], mat[,2], decreasing = TRUE), ])
+expect_identical(sort_desc(mat, 1,2), mat[order(mat[,1], mat[,2], decreasing = TRUE), ])
+
+expect_error(sort_desc(mat, "v1"))
+
+expect_identical(sort_asc(mat[,1,drop = FALSE], 1), mat[,1,drop = FALSE][order(mat[,1], decreasing = FALSE), , drop = FALSE])
+
+colnames(mat) = paste0('v',1:3)
+expect_identical(sort_asc(mat, "v1"), mat[order(mat[,1], decreasing = FALSE), ])
+expect_identical(sort_desc(mat, 1,"v2"), mat[order(mat[,1], mat[,2], decreasing = TRUE), ])
+expect_identical(sort_desc(mat, list(1,"v2")), mat[order(mat[,1], mat[,2], decreasing = TRUE), ])
+
