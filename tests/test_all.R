8aae1959cd4af659321e16c00c4cd4a5ec1592c0
Gregory Demin <gdemin@gmail.com>
2016-10-12 03:12:46 +0300

    Exclude test_all.R from package building

--- tests/test_all.R
+++ tests/test_all.R
@@ -1,13 +1,13 @@
-if( require(testthat) && 
-    require(dplyr)  && 
-    require(stats) && 
-    require(utils) && 
-    require(foreign) && 
-    require(expss)) {
-    lapply(dir("testthat/", full.names = TRUE, pattern = "\\.R$"), function(path) {
-        data("iris")
-        data("mtcars")
-        source(path, chdir = TRUE, local = TRUE)
-        rm(list = ls())
-    })
-}
+library(testthat) 
+library(dplyr) 
+library(stats)  
+library(utils)  
+library(foreign)  
+library(expss) 
+lapply(dir("testthat/", full.names = TRUE, pattern = "\\.R$"), function(path) {
+    data("iris")
+    data("mtcars")
+    source(path, chdir = TRUE, local = TRUE)
+    rm(list = ls())
+})
+
