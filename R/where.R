08d72796687c4d19817ca5a8e3baaee04c9e618b
Gregory Demin <gdemin@gmail.com>
2016-12-25 04:02:50 +0300

    Add methods and tests for `where` for vectors/matrices/lists

--- R/where.R
+++ R/where.R
@@ -1,17 +1,20 @@
-#' Subsetting Data Frames
+#' Subset (filter) data.frames/matrices/vectors/lists
 #'
-#' \code{cond} will be evaluated in the context of the data frame, so columns can be referred to (by
-#' name) as variables in the expression (see the examples).
-#' \code{.where} is version for working with default dataset. See \link{default_dataset}.
-#'  \code{\%where\%} is infix function with the same functional. See examples. There is a 
-#' special constant \code{.n} which equals to number of cases in \code{data} for
-#' usage in \code{cond} expression.
-#' 
-#' @param data data.frame to be subsetted
-#' @param cond logical or numeric expression indicating elements or rows to
-#'   keep: missing values (NA) are taken as false.
+#' For the data frame \code{cond} will be evaluated in the data.frame's context.
+#' So columns can be referred as variables in the expression (see the examples).
+#' If \code{data} is list then \code{where} will be applied to each element of
+#' the list. For other types there is no non-standard evaluation. There is a
+#' special constant \code{.N} which equals to number of rows in \code{data} for
+#' usage in \code{cond} expression. \code{\%where\%} is infix function with the
+#' same functional. See examples. \code{.where} is version for working with
+#' default dataset. See \link{default_dataset}.
+#'  
+#' @param data data.frame/matrix/vector/list to be subsetted
+#' @param cond logical or numeric expression indicating elements or rows to 
+#'   keep: missing values (NA) are taken as false. If \code{data} is data.frame
+#'   then \code{cond} will be evaluated in the scope of the \code{data}.
 #'
-#' @return data.frame which contains just selected rows.
+#' @return data.frame/matrix/vector/list which contains just selected rows.
 #' @export
 #'
 #' @examples
@@ -27,34 +30,50 @@
 #' 
 #' iris %where% 1:5
 #' 
-#' # example of .n usage. Very artificial examples
+#' # example of .N usage. 
 #' set.seed(42)
-#' train = iris %where% sample(.n, 100)
+#' train = iris %where% sample(.N, 100)
 #' str(train)
 #' 
 #' set.seed(42)
-#' test = iris %where% -sample(.n, 100)
+#' test = iris %where% -sample(.N, 100)
 #' str(test)
+#' 
+#' # list example
+#' set.seed(123)
+#' rand_matr = matrix(sample(10, 60, replace = TRUE), ncol = 3)
+#' rand_vec = sample(10, 20, replace = TRUE)
+#' my_list = list(iris, rand_matr, rand_vec)
+#' # two random elements from the each list item
+#' where(my_list, sample(.N, 2))
 where = function (data, cond) {
     UseMethod("where")
 }
 
 #' @export
 where.data.frame = function (data, cond) {
-    e = evalq(environment(), data, parent.frame())
-    e$.n = nrow(data)
     cond = substitute(cond)
-    cond = eval(cond, e)
-    if (!is.logical(cond) && !is.numeric(cond)){ 
-        stop("'cond' must be logical or numeric.")
-    }    
-    if(is.logical(cond)) cond = cond & !is.na(cond)
-    data[cond,, drop = FALSE]
+    eval(bquote(where_helper(data, .(cond))))
 }
+
+#' @export
+where.default = function (data, cond) {
+    cond = substitute(cond)
+    eval(bquote(where_helper(data, .(cond))))
+}
+
+
+#' @export
+where.list = function (data, cond) {
+    cond = substitute(cond)
+    eval(bquote(lapply(data, where, .(cond))))
+}
+
 
 #' @rdname where
 #' @export
 '%where%' = function(data, cond){
+    cond = substitute(cond)
     eval(bquote(where(data, .(cond))))
 }
 
@@ -62,49 +81,33 @@ where.data.frame = function (data, cond) {
 #' @rdname where
 #' @export
 .where = function (cond) {
+    cond = substitute(cond)
     reference = suppressMessages(default_dataset() )
     data = ref(reference)
-    e = evalq(environment(), data, parent.frame())
-    e$.n = nrow(data)
+    data = eval(bquote(where(data, .(cond))))
+    ref(reference) = data 
+    invisible(data)
+}
+
+where_helper = function(data, cond){
     cond = substitute(cond)
+    if(is.data.frame(data)){
+        e = evalq(environment(), data, parent.frame())
+    } else {
+        e = evalq(environment(), new.env(), parent.frame())
+    }
+    e$.n = NROW(data)
+    e$.N = NROW(data)
+    lockBinding(".n", e)
+    lockBinding(".N", e)
     cond = eval(cond, e)
     if (!is.logical(cond) && !is.numeric(cond)){ 
         stop("'cond' must be logical or numeric.")
     }    
     if(is.logical(cond)) cond = cond & !is.na(cond)
-    new_data = data[cond,, drop = FALSE]
-    ref(reference) = new_data
-    invisible(NULL)
-}
-
-#' @export
-where.default = function (data, cond) {
-    e = evalq(environment(), parent.frame())
-    e$.n = length(data)
-    cond = substitute(cond)
-    cond = eval(cond, e)
-    if (!is.logical(cond) && !is.numeric(cond)){
-        stop("'cond' must be logical or numeric.")
-    }
-    if(is.logical(cond)) cond = cond & !is.na(cond)
-    data[cond]
-}
-
-#' @export
-where.matrix = function (data, cond) {
-    e = evalq(environment(), parent.frame())
-    e$.n = nrow(data)
-    cond = substitute(cond)
-    cond = eval(cond, e)
-    if (!is.logical(cond) && !is.numeric(cond)){
-        stop("'cond' must be logical or numeric.")
-    }
-    if(is.logical(cond)) cond = cond & !is.na(cond)
-    data[cond, drop = FALSE]
-}
-
-#' @export
-where.list = function (data, cond) {
-    cond = substitute(cond)
-    eval(bquote(lapply(data, where, .(cond))))
+    if(is.matrix(data) || is.data.frame(data)){
+        data[cond,, drop = FALSE]    
+    }  else {
+        data[cond]
+    }  
 }
--- tests/testthat/test_where.R
+++ tests/testthat/test_where.R
@@ -6,11 +6,18 @@ expect_identical(where(iris, Species == "setosa"), iris[iris$Species == "setosa"
 expect_identical(where(iris, 1:5), iris[1:5, ])
 
 expect_identical(iris %where% (Species == "setosa"), iris[iris$Species == "setosa", ])
+expect_identical(iris %where% (iris$Species == "setosa"), iris[iris$Species == "setosa", ])
+
+
+
+a = 150
+# expect_identical(iris %where% (a < 51), iris[iris$Species == "setosa", ])
 expect_identical(iris %where% 1:5, iris[1:5, ])
 
 d_iris = iris
 
 default_dataset(d_iris)
+
 
 .where(Species == "setosa")
 expect_identical(d_iris, iris[iris$Species == "setosa", ])
@@ -28,11 +35,86 @@ expect_identical(where(iris, sample(.n, 10)), iris[rows, ])
 set.seed(123)
 expect_identical(where(iris, -sample(.n, 10)), iris[-rows, ])
 
+
+set.seed(123)
+expect_identical(where(iris, sample(.N, 10)), iris[rows, ])
+set.seed(123)
+expect_identical(where(iris, -sample(.N, 10)), iris[-rows, ])
+
 set.seed(123)
 expect_identical(iris %where% sample(.n, 10), iris[rows, ])
 set.seed(123)
 expect_identical(iris %where% -sample(.n, 10), iris[-rows, ])
 
+set.seed(123)
+expect_identical(iris %where% sample(.N, 10), iris[rows, ])
+set.seed(123)
+expect_identical(iris %where% -sample(.N, 10), iris[-rows, ])
+
 expect_error(.where("a"))
 expect_error(where(iris, "a"))
 expect_error(iris %where% "a")
+
+#### vectors
+set.seed(123)
+rand_vec = sample(10, 20, replace = TRUE)
+
+set.seed(123)
+rows = sample(20, 10)
+
+set.seed(123)
+expect_identical(where(rand_vec, sample(.N, .N/2)), rand_vec[rows])
+set.seed(123)
+expect_identical(where(rand_vec, sample(.n, .n/2)), rand_vec[rows])
+
+# expect_identical(where(rand_vec, rand_vec>5), rand_vec[rand_vec>5])
+
+set.seed(123)
+rand_matr = matrix(sample(10, 60, replace = TRUE), ncol = 3)
+
+set.seed(123)
+rows = sample(20, 10)
+
+set.seed(123)
+expect_identical(where(rand_matr, sample(.N, .N/2)), rand_matr[rows,])
+set.seed(123)
+expect_identical(where(rand_matr, sample(.n, .n/2)), rand_matr[rows,])
+
+colnames(rand_matr) = c("v1", "v2", "v3")
+expect_error(where(rand_matr, v1<5))
+# expect_identical(where(rand_matr, rand_matr[,"v1"]<5), rand_matr[rand_matr[,"v1"]<5, ])
+
+expect_identical(
+    where(list(iris, rand_vec, rand_matr), 1:2),
+    list(iris[1:2,], rand_vec[1:2], rand_matr[1:2,])
+)
+
+expect_identical(
+    where(list(iris, rand_vec, rand_matr), 1:2),
+    list(iris[1:2,], rand_vec[1:2], rand_matr[1:2,])
+)
+
+
+cond = iris$Petal.Length>1.5
+expect_identical(
+    where(list(iris, iris, iris), Petal.Length>1.5),
+    list(iris[cond,], iris[cond,], iris[cond,])
+)
+
+set.seed(123)
+cond1 = sample(nrow(iris), 3)
+cond2 = sample(nrow(iris), 3)
+cond3 = sample(nrow(iris), 3)
+set.seed(123)
+expect_identical(
+    where(list(iris, iris, iris), sample(.N, 3)),
+    list(iris[cond1,], iris[cond2,], iris[cond3,])
+)
+
+set.seed(123)
+expect_identical(
+    where(list(iris, iris, iris), sample(.n, 3)),
+    list(iris[cond1,], iris[cond2,], iris[cond3,])
+)
+
+expect_identical(where(1:3, c(TRUE, FALSE, NA)), 1L)
