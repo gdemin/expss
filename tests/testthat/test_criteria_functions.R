8bc80f324ca54d3560b902c1f2693113a043f1f1
Gregory Demin <gdemin@gmail.com>
2017-01-02 04:06:15 +0300

    Add tests for logical operators between criteria and logicals

--- tests/testthat/test_criteria_functions.R
+++ tests/testthat/test_criteria_functions.R
@@ -9,3 +9,10 @@ expect_equal((6 & gt(5))(6), TRUE)
 
 expect_equal((gt(5) & 6)(5), FALSE)
 expect_equal((gt(5) & 6)(6), TRUE)
+
+a = 1:4
+expect_equal((eq(4) & (a<2))(a), rep(FALSE, 4))
+expect_equal((eq(4) | (a<2))(a), c(TRUE, FALSE, FALSE, TRUE))
+
+expect_equal(((a<2) & eq(4))(a), rep(FALSE, 4))
+expect_equal(((a<2) | eq(4))(a), c(TRUE, FALSE, FALSE, TRUE))
