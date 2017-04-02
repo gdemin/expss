db367fc14f5fa24b7ca64c052cc92cd8ddd0254b
Gregory Demin <gdemin@gmail.com>
2017-04-03 02:28:23 +0300

    Update tests for 'drop_unused_labels'

--- tests/testthat/test_drop_unused_labels.R
+++ tests/testthat/test_drop_unused_labels.R
@@ -17,3 +17,36 @@ val_lab(b) = autonum(
 )
 
 expect_identical(drop_unused_labels(a), b)
+
+a = as.double(c(1:2, NA))
+var_lab(a) = "my_a"
+val_lab(a) = autonum(
+    "one
+    two
+    three
+    "
+)
+
+d = 1:3
+val_lab(d) = autonum(
+    "one
+    two
+    three
+    "
+)
+
+b = a
+val_lab(b) = autonum(
+    "one
+    two
+    "
+)
+
+expect_identical(drop_unused_labels(dtfrm(a, d)), dtfrm(a = b, d))
+expect_identical(drop_unused_labels(list(a, d)), list(b, d))
+
+expect_identical(drop_unused_labels(mrset(a, d)), mrset(a, d))
+
+new_mrset = mrset(a, d)
+add_val_lab(new_mrset) = c(hs = 99L)             
+expect_identical(drop_unused_labels(new_mrset), mrset(a, d))
