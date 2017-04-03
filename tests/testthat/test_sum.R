fbd33ef603f1b78b97d432da3ea3b327914d9bd1
Gregory Demin <gdemin@gmail.com>
2017-04-04 00:50:32 +0300

    Update tests for 'max_row'/'min_row'

--- tests/testthat/test_sum.R
+++ tests/testthat/test_sum.R
@@ -27,10 +27,10 @@ expect_equal(min_row(a, b, d), c(1,2,1))
 expect_equal(min_col(a, b, d), c(a = 1, b = 1, d = NA))
 
 #######
-expect_equal(max_row(iris), apply(iris, 1, max))
+expect_equal(max_row(iris[,-5]), apply(iris[,-5], 1, max))
 expect_equal(max_col(iris), apply(iris, 2, max))
 
-expect_equal(min_row(iris), gsub("\\.0$","", apply(iris, 1, min), perl = TRUE))
+expect_equal(min_row(iris[,-5]), apply(iris[,-5], 1, min))
 expect_equal(min_col(iris), apply(iris, 2, min))
 
 temp = cbind(a, b, NA)
