e7c1b4c4175dcb3f4845a2af1c74773095d97b51
Gregory Demin <gdemin@gmail.com>
2017-09-17 23:38:21 +0300

    Add 'sheet' - alias to 'dtfrm'

--- R/dtfrm.R
+++ R/dtfrm.R
@@ -1,13 +1,13 @@
 #' Make data.frame without conversion to factors and without fixing names
 #' 
-#' \code{dtfrm} and \code{as.dtfrm} are shortcuts to \code{data.frame} and 
+#' \code{sheet} and \code{as.sheet} are shortcuts to \code{data.frame} and 
 #' \code{as.data.frame} with stringsAsFactors = FALSE, check.names = FALSE.
 #' \code{lst} creates list with names.
-#' \code{.dtfrm}, \code{.lst} the same as above but work in the scope of default
+#' \code{.sheet}, \code{.lst} the same as above but work in the scope of default
 #' dataset.
 #'
 #' @param ... objects, possibly named
-#' @param x object to be coerced 
+#' @param x object to be coerced to data.frame
 #'
 #' @return data.frame/list
 #' @export
@@ -17,7 +17,7 @@
 #' 
 #' # see the difference
 #' df1 = data.frame(a = letters[1:3], "This is my long name" = 1:3)
-#' df2 = dtfrm(a = letters[1:3], "This is my long name" = 1:3)
+#' df2 = sheet(a = letters[1:3], "This is my long name" = 1:3)
 #' 
 #' str(df1)
 #' str(df2)
@@ -35,16 +35,16 @@
 #' data(iris)
 #' default_dataset(iris)
 #' 
-#' .dtfrm(Sepal.Width,  Sepal.Length)
+#' .sheet(Sepal.Width,  Sepal.Length)
 #' .lst(Sepal.Width,  Sepal.Length)
 #' 
-dtfrm = function(...){
+sheet = function(...){
     data.frame(..., check.names = FALSE, stringsAsFactors = FALSE)
 }
 
 #' @export
-#' @rdname dtfrm
-as.dtfrm = function(x, ...) {
+#' @rdname sheet
+as.sheet = function(x, ...) {
     as.data.frame(x, optional = FALSE, check.names = FALSE, ...,
                   cut.names = FALSE, col.names = names(x),
                   stringsAsFactors = FALSE)
@@ -52,7 +52,15 @@ as.dtfrm = function(x, ...) {
 }
 
 #' @export
-#' @rdname dtfrm
+#' @rdname sheet
+dtfrm = sheet
+
+#' @export
+#' @rdname sheet
+as.dtfrm = as.sheet 
+
+#' @export
+#' @rdname sheet
 lst = function(...){
     res = list(...)
     current_names = names(res)
@@ -83,9 +91,13 @@ eval_in_default_dataset = function(...){
 }
 
 #' @export
-#' @rdname dtfrm
+#' @rdname sheet
 .dtfrm = eval_in_default_dataset
 
 #' @export
-#' @rdname dtfrm
+#' @rdname sheet
+.sheet = eval_in_default_dataset
+
+#' @export
+#' @rdname sheet
 .lst =  eval_in_default_dataset
