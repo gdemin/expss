9622d52a3604da88ad90e2e2258a18ea24bb9ddb
Gregory Demin <gdemin@gmail.com>
2019-05-20 01:16:59 +0300

    rewrite 'match_*'/'index_*'. Now they are consistent with criterions and should work faster

--- R/match.R
+++ R/match.R
@@ -47,91 +47,54 @@
 #' index_row(ix, dfs) # c(7,3,3)
 #' @export
 match_row = function(criterion, ...){
-    stopif(length(criterion)==0, "Zero-length 'criterion'.")
-    dfs = dots2data_frame(...) 
-    if(is.function(criterion)){
-        cond = build_criterion(criterion, dfs)
-        apply(cond, 1, function(x) which(x)[1])
-    } else {
-        if(length(criterion)>1){
-            stopif(length(criterion)!=nrow(dfs), "Length of 'criterion' should be
-               1 or equals number of rows of '...' but length(criterion)=",length(criterion),", NROW(...)=", NROW(dfs))
-            unname(unlist(lapply(seq_along(criterion), function(row_num) match(criterion[row_num], unlist(dfs[row_num, ])))))
-        } else {
-            res = apply(dfs, 1, function(row) match(criterion, row))
-            unname(res)
-        }    
-    } 
-    
+    cond = build_condition_matrix(criterion, ..., result = TRUE)
+    res = numeric(nrow(cond))
+    for(i in seq_len(nrow(cond))){
+        res[i] = which(cond[i, ])[1]    
+    }
+    res
 }
 
 
 #' @export
 #' @rdname match_row
 match_col = function(criterion, ...){
-    stopif(length(criterion)==0, "Zero-length 'criterion'.")
-    dfs = dots2data_frame(...)   
-    if(is.function(criterion)){
-        cond = build_criterion(criterion, dfs)
-        res = apply(cond, 2, function(x) which(x)[1])
-        stats::setNames(res, colnames(dfs))
-    } else {
-        if(length(criterion)>1){
-            stopif(length(criterion)!=nrow(dfs), "Length of 'criterion' should be
-                   1 or equals number of columns of '...' but length(criterion)=",length(criterion),", NCOL(...)=", NCOL(dfs))
-            res = unlist(lapply(seq_along(criterion), function(col_num) match(criterion[col_num], dfs[[col_num]])))
-            stats::setNames(res, colnames(dfs))
-        } else {
-            res = apply(dfs, 2, function(col) match(criterion, col))
-            stats::setNames(res, colnames(dfs))
-        }    
-    } 
+    cond = build_condition_matrix(criterion, ..., result = TRUE)
+    res = numeric(ncol(cond))
+    for(i in seq_len(ncol(cond))){
+        res[i] = which(cond[, i])[1]    
+    }
+    names(res) = colnames(cond)
+    res
     
 }
 
 #' @export
 #' @rdname match_row
 index_row = function(index, ...){
-    stopif(length(index)==0, "Zero-length 'index'.")
-    dfs = dots2data_frame(...) 
-    max_index = max_col(index)
-    stopif(!is.na(max_index) && max(index, na.rm = TRUE)>ncol(dfs), "Max 'index' should be
-               less than  or equals to number of columns in  '...' but max(index)=",max(index, na.rm = TRUE),", NCOL(...)=", ncol(dfs))
-    if(length(index)==1){
-        if(!is.na(index)){
-            dfs[[index]]
-        } else {
-           rep(NA, NROW(dfs)) 
-        }
-    } else {
-        stopif(length(index)!=nrow(dfs), "Length of 'index' should be
-               1 or equals number of rows of '...' but length(index)=",length(index),", NROW(...)=", NROW(dfs))
-        
-        # unlist(dfs[row_num, ])[index[row_num]] because if value of index is NA we want NA as a result
-        unname(unlist(lapply(seq_along(index), function(row_num) unlist(dfs[row_num, ])[index[row_num]])))
-    }
+    data = dots2matrix(...)
+    stopifnot(
+        is.numeric(index) || is.logical(index),
+        length(index)==1 || length(index) == nrow(data),
+        all(index<=ncol(data), na.rm = TRUE)
+    )
+    if(is.logical(index)) index = as.numeric(index)
+    matrixStats::rowCollapse(data, index)
     
 }
 
 #' @export
 #' @rdname match_row
 index_col = function(index, ...){
-    stopif(length(index)==0, "Zero-length 'index'.")
-    dfs = dots2data_frame(...)
-    max_index = max_col(index)
-    stopif(!is.na(max_index) && max_index>nrow(dfs), "Max 'index' should be
-               less than or equals to number of rows in  '...' but max(index)=",max(index, na.rm = TRUE),", NROW(...)=", nrow(dfs))
-    if(length(index)==1){
-        if(!is.na(index)){
-            unlist(dfs[index,]) 
-        } else {
-            stats::setNames(rep(NA, NROW(dfs)), colnames(dfs))
-        }    
-    } else {
-        stopif(length(index)!=ncol(dfs), "Length of 'index' should be
-               1 or equals number of columns of '...' but length(index)=",length(index),", NCOL(...)=", NCOL(dfs))
-        res = unlist(lapply(seq_along(index), function(col_num) dfs[[col_num]][index[col_num]]))
-        stats::setNames(res, colnames(dfs))
-    }
+    data = dots2matrix(...)
+    stopifnot(
+        is.numeric(index) || is.logical(index),
+        length(index)==1 || length(index) == ncol(data),
+        all(index<=nrow(data), na.rm = TRUE)
+    )
+    if(is.logical(index)) index = as.numeric(index)
+    res = matrixStats::colCollapse(data, index)
+    names(res) = colnames(data)
+    res
     
 }
--- tests/testthat/test_match.R
+++ tests/testthat/test_match.R
@@ -13,30 +13,20 @@ expect_equal(match_row(gt(2), a), c(2, 2, 1))
 expect_equal(match_col(gt(2), a), c(V1 = 3, V2 = 1, V3 = 1))
 
 V1 = 1:3
-V2 = 4:6
-V3 = 7:9
+V2 = 3:1
+V3 = 9:7
 
-expect_equal(match_row(1, V1, V2, V3), c(1, NA, NA))
-expect_equal(match_col(1, V1, V2, V3), c(V1 = 1, V2 = NA, V3 = NA))
+expect_equal(match_row(1, V1, V2, V3), c(1, NA, 2))
+expect_equal(match_col(1, V1, V2, V3), c(1, 3, NA))
 
 
-expect_equal(match_row(c(1, 5, 9), V1, V2, V3), c(1, 2, 3))
-expect_equal(match_col(c(1, 5, 9), V1, V2, V3), c(V1 = 1, V2 = 2, V3 = 3))
+expect_equal(match_row(c(3, 2, 1), V1, V2, V3), c(1, 1, 1))
+expect_equal(match_row(eq(c(3, 2, 1)), V1, V2, V3), c(2, 1, 2))
 
-expect_error(match_row(c(1, 5), V1, V2, V3))
-expect_error(match_col(c(1, 5), V1, V2, V3))
+expect_equal(match_col(c(2, 9, 8), V1, V2, V3), c(2, 2, 1))
 
-expect_error(match_row(c(1, 5, 9, 9), V1, V2, V3))
-expect_error(match_col(c(1, 5, 9, 9), V1, V2, V3))
-
-expect_equal(match_row(gt(2), V1, V2, V3), c(2, 2, 1))
-expect_equal(match_col(gt(2), V1, V2, V3), c(V1 = 3, V2 = 1, V3 = 1))
-
-expect_error(match_row(NULL, V1, V2, V3))
-expect_error(match_col(NULL, V1, V2, V3))
-
-expect_error(match_row(numeric(0), V1, V2, V3))
-expect_error(match_col(numeric(0), V1, V2, V3))
+expect_equal(match_row(gt(2), V1, V2, V3), c(2, 3, 1))
+expect_equal(match_col(gt(2), V1, V2, V3), c(3, 1, 1))
 
 
 context("index")
@@ -44,10 +34,10 @@ context("index")
 a = as.data.frame(matrix(1:9, ncol = 3))
 
 expect_equal(index_row(1, a), c(1, 2, 3))
-expect_equal(index_col(1, a), c(V1 = 1, V2 = 4, V3 = 7))
+expect_equal(index_col(1, a), c(V1=1, V2 = 4, V3 = 7))
 
-expect_equal(index_row(NA, a), c(NA, NA, NA))
-expect_equal(index_col(NA, a), c(V1 = NA, V2 = NA, V3 = NA))
+expect_equal(index_row(NA, a), 1*c(NA, NA, NA))
+expect_equal(index_col(NA, a), 1*c(V1 = NA, V2 = NA, V3 = NA))
 
 
 expect_equal(index_row(c(1, NA, 2), a), c(1, NA, 6))
@@ -58,14 +48,14 @@ V2 = 4:6
 V3 = 7:9
 
 expect_equal(index_row(1,  V1, V2, V3), c(1, 2, 3))
-expect_equal(index_col(1,  V1, V2, V3), c(V1 = 1, V2 = 4, V3 = 7))
+expect_equal(index_col(1,  V1, V2, V3), c(1, 4, 7))
 
-expect_equal(index_row(NA, V1, V2, V3), c(NA, NA, NA))
-expect_equal(index_col(NA, V1, V2, V3), c(V1 = NA, V2 = NA, V3 = NA))
+expect_equal(index_row(NA, V1, V2, V3), 1*c(NA, NA, NA))
+expect_equal(index_col(NA, V1, V2, V3), 1*c(NA, NA, NA))
 
 
 expect_equal(index_row(c(1, NA, 2),  V1, V2, V3), c(1, NA, 6))
-expect_equal(index_col(c(1, 3, NA),  V1, V2, V3), c(V1 = 1, V2 = 6, V3 = NA))
+expect_equal(index_col(c(1, 3, NA),  V1, V2, V3), c(1, 6, NA))
 
 expect_error(index_row(1:2,  V1, V2, V3))
 expect_error(index_col(1:2,  V1, V2, V3))
@@ -86,9 +76,9 @@ v2 = 2:4
 v3 = 7:5
 
 # postions of 1,3,5 in rows
-expect_equal(match_row(c(1, 3, 5), v1, v2, v3),1:3)
+expect_equal(match_row(eq(c(1, 3, 5)), v1, v2, v3),1:3)
 # postions of 1,3,5 in columnss
-expect_equal(match_col(1, v1, v2, v3), c(v1 = 1, v2 = NA, v3 = NA))
+expect_equal(match_col(1, v1, v2, v3), c(1, NA, NA))
 
 # postion of first value greater than 2
 ix = match_row(gt(2), v1, v2, v3) 
@@ -100,7 +90,7 @@ expect_equal(index_row(ix, v1, v2, v3), c(7,3,3))
 dfs = data.frame(v1, v2, v3)
 
 # postions of 1,3,5 in rows
-expect_equal(match_row(c(1, 3, 5), dfs), 1:3)
+expect_equal(match_row(eq(c(1, 3, 5)), dfs), 1:3)
 # postions of 1,3,5 in columnss
 expect_equal(match_col(1, dfs), c(v1 = 1, v2 = NA, v3 = NA))
 
